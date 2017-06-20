97cb23505103d44db9a941ffa64d38bd20947e45
CKrawczyk <coleman.krawczyk@gmail.com>
2017-06-20 15:17:27 +0100

    Add tests for question reducer

--- panoptes_aggregation/reducers/__init__.py
+++ panoptes_aggregation/reducers/__init__.py
@@ -1,10 +1,13 @@
 from . import cluster_points
+from . import question_reducer
 from .process_kwargs import process_kwargs
 
 reducers = {
-    'point_reducer': cluster_points.reducer_request
+    'point_reducer': cluster_points.reducer_request,
+    'question_reducer': question_reducer.reducer_request
 }
 
 reducer_base = {
-    'point_reducer': cluster_points.reducer_base
+    'point_reducer': cluster_points.reducer_base,
+    'question_reducer': question_reducer.reducer_base
 }
--- panoptes_aggregation/reducers/question_reducer.py
+++ panoptes_aggregation/reducers/question_reducer.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from .process_kwargs import process_kwargs
 
 DEFAULTS = {
     'pairs': {'default': False, 'type': bool}
@@ -12,7 +13,7 @@ def process_data(data, pairs=False):
             new_key = '+'.join(sorted(d))
             data_out.append(Counter({new_key: 1}))
         else:
-            data_out.append(Coutner(d))
+            data_out.append(Counter(d))
     return data_out
 
 
@@ -24,4 +25,10 @@ def count_votes(votes_list):
 def reducer_request(request):
     kwargs = process_kwargs(request.args, DEFAULTS)
     data = process_data([d['data'] for d in request.get_json()], **kwargs)
-    return count_votes(data, **kwargs)
+    return count_votes(data)
+
+
+def reducer_base(data_in, **kwargs):
+    kwargs_parse = process_kwargs(kwargs, DEFAULTS)
+    data = process_data(data_in, **kwargs_parse)
+    return count_votes(data)
