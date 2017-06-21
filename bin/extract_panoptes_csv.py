6839c66e483f4c50f97c9b1ccfbea161e8081972
CKrawczyk <coleman.krawczyk@gmail.com>
2017-06-21 15:09:43 +0100

    make external code also importable

--- bin/extract_panoptes_csv.py
+++ bin/extract_panoptes_csv.py
@@ -1,88 +1,98 @@
 #!/usr/bin/env python
 
-import argparse
 from collections import OrderedDict
 import copy
 from panoptes_aggregation import extractors
 from panoptes_aggregation.csv_utils import flatten_data
 import json
 import math
+import io
 import os
 import pandas
 import progressbar
 
-parser = argparse.ArgumentParser(description="extract data from panoptes classifications based on the workflow")
-parser.add_argument("classification_csv", help="the classificaiton csv file containing the panoptes data dump", type=argparse.FileType('r'))
-parser.add_argument("workflow_csv", help="the csv file containing the workflow data", type=argparse.FileType('r'))
-parser.add_argument("workflow_id", help="the workflow ID you would like to extract", type=int)
-parser.add_argument("-v", "--version", help="the workflow version to extract", type=int, default=1)
-parser.add_argument("-H", "--human", help="switch to make the data column labels use the task and question labels instead of generic labels", action="store_true")
-parser.add_argument("-o", "--output", help="the base name for output csv file to store the annotation extractions (one file will be created for each extractor used)", type=str, default="extractions.csv")
-args = parser.parse_args()
 
-with args.workflow_csv as workflow_csv:
-    workflows = pandas.read_csv(workflow_csv)
+def extract_csv(classification_csv, workflow_csv, workflow_id, version=1, human=False, output='extractions'):
+    if not isinstance(workflow_csv, io.IOBase):
+        workflow_csv = open(workflow_csv, 'r')
 
-wdx = (workflows.workflow_id == args.workflow_id) & (workflows.version == args.version)
-if wdx.sum() == 0:
-    raise IndexError('workflow ID and workflow version combination does not exist')
-if wdx.sum() > 1:
-    raise IndexError('workflow ID and workflow version combination is not unique')
-workflow = workflows[wdx].iloc[0]
-workflow_tasks = json.loads(workflow.tasks)
-extractor_config = extractors.workflow_extractor_config(workflow_tasks)
+    with workflow_csv as workflow_csv_in:
+        workflows = pandas.read_csv(workflow_csv_in)
 
-blank_extracted_data = OrderedDict([
-    ('classification_id', []),
-    ('user_name', []),
-    ('user_id', []),
-    ('workflow_id', []),
-    ('task', []),
-    ('created_at', []),
-    ('subject_id', []),
-    ('extractor', []),
-    ('data', [])
-])
+    wdx = (workflows.workflow_id == workflow_id) & (workflows.version == version)
+    if wdx.sum() == 0:
+        raise IndexError('workflow ID and workflow version combination does not exist')
+    if wdx.sum() > 1:
+        raise IndexError('workflow ID and workflow version combination is not unique')
+    workflow = workflows[wdx].iloc[0]
+    workflow_tasks = json.loads(workflow.tasks)
+    extractor_config = extractors.workflow_extractor_config(workflow_tasks)
 
-extracted_data = {}
+    blank_extracted_data = OrderedDict([
+        ('classification_id', []),
+        ('user_name', []),
+        ('user_id', []),
+        ('workflow_id', []),
+        ('task', []),
+        ('created_at', []),
+        ('subject_id', []),
+        ('extractor', []),
+        ('data', [])
+    ])
 
-with args.classification_csv as classification_csv:
-    classifications = pandas.read_csv(classification_csv)
+    extracted_data = {}
 
-widgets = [
-    'Extracting: ',
-    progressbar.Percentage(),
-    ' ', progressbar.Bar(),
-    ' ', progressbar.ETA()
-]
-pbar = progressbar.ProgressBar(widgets=widgets, max_value=len(classifications))
-pbar.start()
-for cdx, classification in classifications.iterrows():
-    if (classification.workflow_id != args.workflow_id) or (math.floor(classification.workflow_version) != args.version):
+    with classification_csv as classification_csv_in:
+        classifications = pandas.read_csv(classification_csv_in)
+
+    widgets = [
+        'Extracting: ',
+        progressbar.Percentage(),
+        ' ', progressbar.Bar(),
+        ' ', progressbar.ETA()
+    ]
+    pbar = progressbar.ProgressBar(widgets=widgets, max_value=len(classifications))
+    pbar.start()
+    for cdx, classification in classifications.iterrows():
+        if (classification.workflow_id != workflow_id) or (math.floor(classification.workflow_version) != version):
+            pbar.update(cdx + 1)
+            continue
+        annotations_by_extractor = extractors.filter_annotations(json.loads(classification.annotations), extractor_config, human=human)
+        for extractor_name, annotations_list in annotations_by_extractor.items():
+            for annotations in annotations_list:
+                if extractor_name in extractors.extractors_base:
+                    extract = extractors.extractors_base[extractor_name]({'annotations': [annotations]})
+                    extracted_data.setdefault(extractor_name, copy.deepcopy(blank_extracted_data))
+                    extracted_data[extractor_name]['classification_id'].append(classification.classification_id)
+                    extracted_data[extractor_name]['user_name'].append(classification.user_name)
+                    extracted_data[extractor_name]['user_id'].append(classification.user_id)
+                    extracted_data[extractor_name]['workflow_id'].append(classification.workflow_id)
+                    extracted_data[extractor_name]['task'].append(annotations['task'])
+                    extracted_data[extractor_name]['created_at'].append(classification.created_at)
+                    extracted_data[extractor_name]['subject_id'].append(classification.subject_ids)
+                    extracted_data[extractor_name]['extractor'].append(extractor_name)
+                    extracted_data[extractor_name]['data'].append(extract)
         pbar.update(cdx + 1)
-        continue
-    annotations_by_extractor = extractors.filter_annotations(json.loads(classification.annotations), extractor_config, human=args.human)
-    for extractor_name, annotations_list in annotations_by_extractor.items():
-        for annotations in annotations_list:
-            if extractor_name in extractors.extractors_base:
-                extract = extractors.extractors_base[extractor_name]({'annotations': [annotations]})
-                extracted_data.setdefault(extractor_name, copy.deepcopy(blank_extracted_data))
-                extracted_data[extractor_name]['classification_id'].append(classification.classification_id)
-                extracted_data[extractor_name]['user_name'].append(classification.user_name)
-                extracted_data[extractor_name]['user_id'].append(classification.user_id)
-                extracted_data[extractor_name]['workflow_id'].append(classification.workflow_id)
-                extracted_data[extractor_name]['task'].append(annotations['task'])
-                extracted_data[extractor_name]['created_at'].append(classification.created_at)
-                extracted_data[extractor_name]['subject_id'].append(classification.subject_ids)
-                extracted_data[extractor_name]['extractor'].append(extractor_name)
-                extracted_data[extractor_name]['data'].append(extract)
-    pbar.update(cdx + 1)
-pbar.finish()
+    pbar.finish()
 
-# create one flat csv file for each extractor used
-output_path, output_base = os.path.split(args.output)
-ouput_base_name, output_ext = os.path.splitext(output_base)
-for extractor_name, data in extracted_data.items():
-    output_name = os.path.join(output_path, '{0}_{1}.csv'.format(extractor_name, args.output))
-    flat_extract = flatten_data(data)
-    flat_extract.to_csv(output_name, index=False)
+    # create one flat csv file for each extractor used
+    output_path, output_base = os.path.split(output)
+    ouput_base_name, output_ext = os.path.splitext(output_base)
+    for extractor_name, data in extracted_data.items():
+        output_name = os.path.join(output_path, '{0}_{1}.csv'.format(extractor_name, output))
+        flat_extract = flatten_data(data)
+        flat_extract.to_csv(output_name, index=False)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="extract data from panoptes classifications based on the workflow")
+    parser.add_argument("classification_csv", help="the classificaiton csv file containing the panoptes data dump", type=argparse.FileType('r'))
+    parser.add_argument("workflow_csv", help="the csv file containing the workflow data", type=argparse.FileType('r'))
+    parser.add_argument("workflow_id", help="the workflow ID you would like to extract", type=int)
+    parser.add_argument("-v", "--version", help="the workflow version to extract", type=int, default=1)
+    parser.add_argument("-H", "--human", help="switch to make the data column labels use the task and question labels instead of generic labels", action="store_true")
+    parser.add_argument("-o", "--output", help="the base name for output csv file to store the annotation extractions (one file will be created for each extractor used)", type=str, default="extractions")
+    args = parser.parse_args()
+
+    extract_csv(args.classification_csv, args.workflow_csv, args.workflow_id, version=args.version, human=args.human, output=args.output)
--- bin/reduce_panoptes_csv.py
+++ bin/reduce_panoptes_csv.py
@@ -1,69 +1,79 @@
 #!/usr/bin/env python
 
-import argparse
 from collections import OrderedDict
 import copy
 from panoptes_aggregation import reducers
 from panoptes_aggregation.csv_utils import flatten_data, unflatten_data
 import json
 import math
+import io
 import os
 import pandas
 import progressbar
 
-parser = argparse.ArgumentParser(description="reduce data from panoptes classifications based on the extracted data (see extract_panoptes_csv)")
-parser.add_argument("extracted_csv", help="the extracted csv file output from extract_panoptes_csv", type=argparse.FileType('r'))
-parser.add_argument("-F", "--filter", help="how to filter a user makeing multiple classifications for one subject", type=str, choices=['first', 'last', 'all'], default='fisrt')
-parser.add_argument("-k", "--keywords", help="keywords to be passed into the reducer in the form of a json string, e.g. \'{\"eps\": 5.5, \"min_samples\": 3}\'  (note: double quotes must be used inside the brackets)", type=json.loads, default={})
-parser.add_argument("-o", "--output", help="the base name for output csv file to store the reductions", type=str, default="reductions.csv")
-args = parser.parse_args()
 
-with args.extracted_csv as extracted_csv:
-    extracted = pandas.read_csv(extracted_csv, infer_datetime_format=True, parse_dates=['created_at'])
+def reduce_csv(extracted_csv, filter='first', keywords={}, output='reductions'):
+    if not isinstance(extracted_csv, io.IOBase):
+        workflow_csv = open(workflow_csv, 'r')
 
-extracted.sort_values(['subject_id', 'created_at'], inplace=True)
+    with extracted_csv as extracted_csv:
+        extracted = pandas.read_csv(extracted_csv, infer_datetime_format=True, parse_dates=['created_at'])
 
-subjects = extracted.subject_id.unique()
-tasks = extracted.task.unique()
-workflow_id = extracted.workflow_id.iloc[0]
-extractor_name = extracted.extractor.iloc[0]
-reducer_name = extractor_name.replace('extractor', 'reducer')
+    extracted.sort_values(['subject_id', 'created_at'], inplace=True)
 
-reduced_data = OrderedDict([
-    ('subject_id', []),
-    ('workflow_id', []),
-    ('task', []),
-    ('reducer', []),
-    ('data', [])
-])
+    subjects = extracted.subject_id.unique()
+    tasks = extracted.task.unique()
+    workflow_id = extracted.workflow_id.iloc[0]
+    extractor_name = extracted.extractor.iloc[0]
+    reducer_name = extractor_name.replace('extractor', 'reducer')
 
-widgets = [
-    'Reducing: ',
-    progressbar.Percentage(),
-    ' ', progressbar.Bar(),
-    ' ', progressbar.ETA()
-]
+    reduced_data = OrderedDict([
+        ('subject_id', []),
+        ('workflow_id', []),
+        ('task', []),
+        ('reducer', []),
+        ('data', [])
+    ])
 
-pbar = progressbar.ProgressBar(widgets=widgets, max_value=len(subjects))
-pbar.start()
-for sdx, subject in enumerate(subjects):
-    idx = extracted.subject_id == subject
-    for task in tasks:
-        jdx = extracted.task == task
-        classifications = extracted[idx & jdx]
-        if args.filter in ['frist', 'last']:
-            classifications.drop_duplicates(['user_name'], keep=args.filter, inplace=True)
-        data = [unflatten_data(c) for cdx, c in classifications.iterrows()]
-        reduced_data['subject_id'].append(subject)
-        reduced_data['workflow_id'].append(workflow_id)
-        reduced_data['task'].append(task)
-        reduced_data['reducer'].append(reducer_name)
-        reduced_data['data'].append(reducers.reducer_base[reducer_name](data, **args.keywords))
-    pbar.update(sdx + 1)
-pbar.finish()
+    widgets = [
+        'Reducing: ',
+        progressbar.Percentage(),
+        ' ', progressbar.Bar(),
+        ' ', progressbar.ETA()
+    ]
 
-output_path, output_base = os.path.split(args.output)
-ouput_base_name, output_ext = os.path.splitext(output_base)
-output_name = os.path.join(output_path, '{0}_{1}.csv'.format(reducer_name, ouput_base_name))
-flat_reduced_data = flatten_data(reduced_data)
-flat_reduced_data.to_csv(output_name, index=False)
+    pbar = progressbar.ProgressBar(widgets=widgets, max_value=len(subjects))
+    pbar.start()
+    for sdx, subject in enumerate(subjects):
+        idx = extracted.subject_id == subject
+        for task in tasks:
+            jdx = extracted.task == task
+            classifications = extracted[idx & jdx]
+            if filter in ['frist', 'last']:
+                classifications.drop_duplicates(['user_name'], keep=filter, inplace=True)
+            data = [unflatten_data(c) for cdx, c in classifications.iterrows()]
+            reduced_data['subject_id'].append(subject)
+            reduced_data['workflow_id'].append(workflow_id)
+            reduced_data['task'].append(task)
+            reduced_data['reducer'].append(reducer_name)
+            reduced_data['data'].append(reducers.reducer_base[reducer_name](data, **keywords))
+        pbar.update(sdx + 1)
+    pbar.finish()
+
+    output_path, output_base = os.path.split(output)
+    ouput_base_name, output_ext = os.path.splitext(output_base)
+    output_name = os.path.join(output_path, '{0}_{1}.csv'.format(reducer_name, ouput_base_name))
+    flat_reduced_data = flatten_data(reduced_data)
+    flat_reduced_data.to_csv(output_name, index=False)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="reduce data from panoptes classifications based on the extracted data (see extract_panoptes_csv)")
+    parser.add_argument("extracted_csv", help="the extracted csv file output from extract_panoptes_csv", type=argparse.FileType('r'))
+    parser.add_argument("-F", "--filter", help="how to filter a user makeing multiple classifications for one subject", type=str, choices=['first', 'last', 'all'], default='fisrt')
+    parser.add_argument("-k", "--keywords", help="keywords to be passed into the reducer in the form of a json string, e.g. \'{\"eps\": 5.5, \"min_samples\": 3}\'  (note: double quotes must be used inside the brackets)", type=json.loads, default={})
+    parser.add_argument("-o", "--output", help="the base name for output csv file to store the reductions", type=str, default="reductions")
+    args = parser.parse_args()
+
+    reduce_csv(args.extracted_csv, filter=args.filter, keywords=args.keywords, output=args.output)
