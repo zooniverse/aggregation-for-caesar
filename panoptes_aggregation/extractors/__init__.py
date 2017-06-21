c2dbe0eb205c1085e135ae0f7506001e3b8479b7
CKrawczyk <coleman.krawczyk@gmail.com>
2017-06-21 16:30:53 +0100

    add survey extractor

--- panoptes_aggregation/extractors/__init__.py
+++ panoptes_aggregation/extractors/__init__.py
@@ -1,14 +1,17 @@
 from . import point_extractor
 from . import question_extractor
+from . import survey_extractor
 from .workflow_extractor_config import workflow_extractor_config
 from .filter_annotations import filter_annotations
 
 extractors = {
     'point_extractor': point_extractor.extractor_request,
-    'question_extractor': question_extractor.extractor_request
+    'question_extractor': question_extractor.extractor_request,
+    'survey_extractor': survey_extractor.extractor_request
 }
 
 extractors_base = {
     'point_extractor': point_extractor.classification_to_extract,
-    'question_extractor': question_extractor.classification_to_extract
+    'question_extractor': question_extractor.classification_to_extract,
+    'survey_extractor': survey_extractor.classification_to_extract
 }
