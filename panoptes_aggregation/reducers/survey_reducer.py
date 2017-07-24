d2cb0439357312e7a7b77c3be74ed66a4bf669b2
CKrawczyk <coleman.krawczyk@gmail.com>
2017-07-24 12:01:04 +0100

    Adds survey reducer
    Note, this makes one reduced value for extracted 'choice'.
    `reduce_panoptes_csv` will have to be updated to make this work
    correctly.

---
+++ panoptes_aggregation/reducers/survey_reducer.py
@@ -0,0 +1,42 @@
+from collections import Counter, OrderedDict
+
+
+def process_data(data):
+    vote_count = len(data)
+    data_out = {}
+    for d in data:
+        choice = d.pop('choice')
+        outer = {}
+        for key, value in d.items():
+            key_split = key.split('.')
+            outer_key = '.'.join(key_split[:-1])
+            outer.setdefault(outer_key, Counter())[key_split[-1]] = int(value)
+        data_out.setdefault(choice, []).append(outer)
+    return data_out, vote_count
+
+
+def count_votes(data, vote_count=0):
+    reduction_list = []
+    for choice, answers in data.items():
+        reduction = OrderedDict()
+        reduction = OrderedDict([
+            ('choice', choice),
+            ('total_vote_count', vote_count),
+            ('choice_count', len(answers))
+        ])
+        for answer in answers:
+            for key, value in answer.items():
+                reduction.setdefault(key, Counter())
+                reduction[key] += value
+        reduction_list.append(reduction)
+    return reduction_list
+
+
+def survey_reducer_request(request):
+    data, vote_count = process_data([d['data'] for d in request.get_json()])
+    return count_votes(data, vote_count=vote_count)
+
+
+def reducer_base(data_in):
+    data, vote_count = process_data(data_in)
+    return count_votes(data, vote_count=vote_count)
