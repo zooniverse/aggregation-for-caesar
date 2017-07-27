32d56577162b75fb9c9f7ad7e4ee387b911181bb
CKrawczyk <coleman.krawczyk@gmail.com>
2017-07-27 13:17:32 +0100

    update requirements in setup.py
    This removes an un-needed import statement in cluster_points.py and
    updates the requirements list in setup.py.  Flask has also been added as
    an extra_requriement.

--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
 from setuptools import setup, find_packages
 
-setup(name='panoptes_aggregation',
-           version='0.1',
-           description='Aggegation code for Zooniverse panoptes projects.',
-           classifiers=['Programming Language :: Python :: 3 :: Only'],
-           url='https://github.com/CKrawczyk/python-reducers-for-caesar',
-           author='Coleman Krawczyk',
-           author_email='coleman@zooniverse.org',
-           test_suite='nose.collector',
-           tests_require=['nose'],
-           scripts=['bin/extract_panoptes_csv.py', 'bin/reduce_panoptes_csv.py'],
-           packages=find_packages(),
-           include_package_data=True,
-           install_requires=[
-               'numpy',
-               'pandas',
-               'progressbar2',
-               'python-slugify',
-               'scikit-image',
-               'scikit-learn',
-               'scipy'
-           ])
+setup(
+    name='panoptes_aggregation',
+    version='0.1',
+    description='Aggegation code for Zooniverse panoptes projects.',
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    url='https://github.com/zooniverse/aggregation-for-caesar',
+    author='Coleman Krawczyk',
+    author_email='coleman@zooniverse.org',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    scripts=['bin/extract_panoptes_csv.py', 'bin/reduce_panoptes_csv.py'],
+    packages=find_packages(),
+    include_package_data=True,
+    extra_require={
+        'online': ['flask']
+    },
+    install_requires=[
+        'numpy',
+        'pandas',
+        'progressbar2',
+        'python-slugify',
+        'scikit-image',
+        'scikit-learn',
+        'scipy',
+        'slugify',
+        'werkzeug'
+    ]
+)
