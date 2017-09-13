9e00d111dcf3e56374319d4b20620de4d39f1083
CKrawczyk <coleman.krawczyk@gmail.com>
2017-09-13 15:08:49 +0100

    Add gutter detection to polygon-text tool
    The x-coordinates of the input line-segments are used to automatically
    detect page gutters as gaps present in *all* annotations.  The clustered
    lines of text will not corss these detected gutters.

--- panoptes_aggregation/reducers/poly_line_text_reducer.py
+++ panoptes_aggregation/reducers/poly_line_text_reducer.py
@@ -21,6 +21,61 @@ DEFAULTS = {
 }
 
 
+def overlap(x, y):
+    '''Check if two line segments overlap
+
+    Parameters
+    ----------
+    x : list
+        A list with the start and end point of the first line segment
+    y : lits
+        A list with the start and end point of the second line segment
+
+    Returns
+    -------
+    overlap : bool
+        True if the two line segments overlap, False otherwise
+    '''
+    return x[1] >= y[0] and y[1] >= x[0]
+
+
+def gutter(lines_in):
+    '''Cluster list of input line segments by what side of
+    the page gutter they are on.
+
+    Parameters
+    ----------
+    lines_in : list
+        A list-of-lists containing one line segment per item. Each line
+        segment should contain only the x-coordinate of each point on the line.
+
+    Returns
+    -------
+    gutter_index : array
+        A numpy array containing the cluster label for each input line. This label
+        idicates what side of the gutter(s) the input line segment is on.
+    '''
+    lines = [[min(l), max(l)] for l in lines_in]
+    overlap_lines = []
+    for ldx, l in enumerate(lines):
+        if ldx == 0:
+            overlap_lines = np.array([l])
+        else:
+            o_lines = np.array([overlap(o, l) for o in overlap_lines])
+            if o_lines.any():
+                comp = np.vstack([overlap_lines[o_lines], l])
+                overlap_lines[o_lines] = [comp.min(), comp.max()]
+                overlap_lines = np.unique(overlap_lines).reshape(-1, 2)
+            else:
+                overlap_lines = np.vstack([overlap_lines, l])
+    overlap_lines.sort(axis=0)
+    gutter_label = -np.ones(len(lines), dtype=int)
+    for odx, o in enumerate(overlap_lines):
+        gdx = np.array([overlap(o, l) for l in lines])
+        gutter_label[gdx] = odx
+    return gutter_label
+
+
 def angle_metric(t1, t2):
     '''A metric for the distance between angles in the [-180, 180] range
 
@@ -128,43 +183,50 @@ def poly_line_text_reducer(data_by_frame, **kwargs):
     metric = kwargs.pop('metric')
     for frame, value in data_by_frame.items():
         reduced_data[frame] = []
-        slope = np.array(value['slope']).reshape(-1, 1)
-        x = np.array(value['x'])
-        y = np.array(value['y'])
-        text = np.array(value['text'])
-        db_slope = DBSCAN(eps=eps_slope, metric=angle_metric, **kwargs).fit(slope)
-        for slope_label in set(db_slope.labels_):
-            if slope_label > -1:
-                sdx = db_slope.labels_ == slope_label
-                xy_slope = np.array(list(zip(np.hstack(x[sdx]), np.hstack(y[sdx]))))
-                text_slope = np.hstack(text[sdx])
-                avg_slope = avg_angle(slope[sdx])
-                c = np.cos(np.deg2rad(-avg_slope))
-                s = np.sin(np.deg2rad(-avg_slope))
-                R = np.array([[c, -s], [s, c]])
-                xy_rotate = np.dot(xy_slope, R.T)
-                lines = xy_rotate[:, 1].reshape(-1, 1)
-                words = xy_rotate[:, 0].reshape(-1, 1)
-                db_lines = DBSCAN(eps=eps_line, metric=metric, **kwargs).fit(lines)
-                for line_label in set(db_lines.labels_):
-                    if line_label > -1:
-                        line_dict = {
-                            'clusters_x': [],
-                            'clusters_y': [],
-                            'clusters_text': [],
-                            'line_slope': avg_slope
-                        }
-                        ldx = db_lines.labels_ == line_label
-                        text_line = text_slope[ldx]
-                        xy_line = xy_slope[ldx]
-                        words_line = words[ldx]
-                        db_words = DBSCAN(eps=eps_word, metric=metric, **kwargs).fit(words_line)
-                        for word_label in set(db_words.labels_):
-                            if word_label > -1:
-                                wdx = db_words.labels_ == word_label
-                                word_x, word_y = xy_line[wdx].mean(axis=0)
-                                line_dict['clusters_x'].append(word_x)
-                                line_dict['clusters_y'].append(word_y)
-                                line_dict['clusters_text'].append(list(text_line[wdx]))
-                        reduced_data[frame].append(line_dict)
+        slope_frame = np.array(value['slope']).reshape(-1, 1)
+        x_frame = np.array(value['x'])
+        y_frame = np.array(value['y'])
+        text_frame = np.array(value['text'])
+        gutter_labels = gutter(x_frame)
+        for gutter_label in set(gutter_labels):
+            gdx = gutter_labels == gutter_label
+            x = x_frame[gdx]
+            y = y_frame[gdx]
+            text = text_frame[gdx]
+            slope = slope_frame[gdx]
+            db_slope = DBSCAN(eps=eps_slope, metric=angle_metric, **kwargs).fit(slope)
+            for slope_label in set(db_slope.labels_):
+                if slope_label > -1:
+                    sdx = db_slope.labels_ == slope_label
+                    xy_slope = np.array(list(zip(np.hstack(x[sdx]), np.hstack(y[sdx]))))
+                    text_slope = np.hstack(text[sdx])
+                    avg_slope = avg_angle(slope[sdx])
+                    c = np.cos(np.deg2rad(-avg_slope))
+                    s = np.sin(np.deg2rad(-avg_slope))
+                    R = np.array([[c, -s], [s, c]])
+                    xy_rotate = np.dot(xy_slope, R.T)
+                    lines = xy_rotate[:, 1].reshape(-1, 1)
+                    words = xy_rotate[:, 0].reshape(-1, 1)
+                    db_lines = DBSCAN(eps=eps_line, metric=metric, **kwargs).fit(lines)
+                    for line_label in set(db_lines.labels_):
+                        if line_label > -1:
+                            line_dict = {
+                                'clusters_x': [],
+                                'clusters_y': [],
+                                'clusters_text': [],
+                                'line_slope': avg_slope
+                            }
+                            ldx = db_lines.labels_ == line_label
+                            text_line = text_slope[ldx]
+                            xy_line = xy_slope[ldx]
+                            words_line = words[ldx]
+                            db_words = DBSCAN(eps=eps_word, metric=metric, **kwargs).fit(words_line)
+                            for word_label in set(db_words.labels_):
+                                if word_label > -1:
+                                    wdx = db_words.labels_ == word_label
+                                    word_x, word_y = xy_line[wdx].mean(axis=0)
+                                    line_dict['clusters_x'].append(word_x)
+                                    line_dict['clusters_y'].append(word_y)
+                                    line_dict['clusters_text'].append(list(text_line[wdx]))
+                            reduced_data[frame].append(line_dict)
     return reduced_data
