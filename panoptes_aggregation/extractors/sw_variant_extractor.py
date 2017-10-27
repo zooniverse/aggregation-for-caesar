16ecfe4671eb70ace505ed89518964eab23a873c
CKrawczyk <coleman.krawczyk@gmail.com>
2017-10-27 14:40:41 +0100

    Add doc string to new extractor and reducer

--- panoptes_aggregation/extractors/sw_variant_extractor.py
+++ panoptes_aggregation/extractors/sw_variant_extractor.py
@@ -1,8 +1,28 @@
+'''
+Shakespeares World Variants Extractor
+-------------------------------------
+This module provides a fuction to extract the `variants` data from
+annotations.
+'''
 from .extractor_wrapper import extractor_wrapper
 
 
 @extractor_wrapper
 def sw_variant_extractor(classification):
+    '''Extract all variants in a classification into one list
+
+    Parameters
+    ----------
+    classification : dict
+        A dictionary containing an `annotations` key that is a list of
+        Shakespeares World annotations
+
+    Returns
+    -------
+    extraction : dict
+        A dictionary with at most one key, `variants` with the list of
+        all variants in the classification
+    '''
     extract = {}
     variants = []
     annotation = classification['annotations'][0]
--- panoptes_aggregation/reducers/sw_variant_reducer.py
+++ panoptes_aggregation/reducers/sw_variant_reducer.py
@@ -1,8 +1,28 @@
+'''
+Shakespeares World Variants Reducer
+-----------------------------------
+This module provides a fuction to reduce the `variants` data from
+extracts.
+'''
 from .reducer_wrapper import reducer_wrapper
 
 
 @reducer_wrapper()
 def sw_variant_reducer(extracts):
+    '''Reduce all variants for a subject into one list
+
+    Parameters
+    ----------
+    extracts : list
+        A list of extracts created by
+        :meth:`panoptes_aggregation.extractors.sw_variant_extractor.sw_variant_extractor`
+
+    Returns
+    -------
+    reduction : dict
+        A dictionary with at most one key, `variants` with the list of
+        all variants in the subject
+    '''
     reduction = {}
     variants = []
     for e in extracts:
