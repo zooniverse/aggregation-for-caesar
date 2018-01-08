aa2078e25cb07c5b4be0e66173b6e7152d0f4aea
CKrawczyk <coleman.krawczyk@gmail.com>
2018-01-08 11:47:55 +0000

    Add test for wrong AnnoTate format
    This test makes sure classifications of the wrong format are correctly
    ignored in the sw_*_extractors.

--- panoptes_aggregation/tests/extractor_tests/test_sw_graphic_extractor.py
+++ panoptes_aggregation/tests/extractor_tests/test_sw_graphic_extractor.py
@@ -99,6 +99,15 @@ classification_blank = {
 
 expected_blank = {}
 
+classification_wrong = {
+    'annotations': [
+        {
+            'value': 1,
+            'task': 'T2'
+        }
+    ]
+}
+
 
 class TextSWGraphicExtractor(unittest.TestCase):
     def test_extract_sw(self):
@@ -136,6 +145,20 @@ class TextSWGraphicExtractor(unittest.TestCase):
     def test_request_blank(self):
         request_kwargs = {
             'data': json.dumps(annotation_by_task(classification_blank)),
+            'content_type': 'application/json'
+        }
+        app = flask.Flask(__name__)
+        with app.test_request_context(**request_kwargs):
+            result = extractors.sw_graphic_extractor(flask.request)
+            self.assertDictEqual(result, expected_blank)
+
+    def test_extract_wrong(self):
+        result = extractors.sw_graphic_extractor(classification_wrong)
+        self.assertDictEqual(result, expected_blank)
+
+    def test_request_wrong(self):
+        request_kwargs = {
+            'data': json.dumps(annotation_by_task(classification_wrong)),
             'content_type': 'application/json'
         }
         app = flask.Flask(__name__)
