a04f4bc4a11bd140c54aec8e091ef9ae8e0e0976
CKrawczyk <coleman.krawczyk@gmail.com>
2018-01-08 11:42:33 +0000

    Make sure annotation.value is a list in sw_*_extractors
    There is at least one classificaion on annotate that has the wrong task
    label and causes the extractors to crash. This makes sure only
    classifications that are the right data type are used.

--- panoptes_aggregation/extractors/sw_graphic_extractor.py
+++ panoptes_aggregation/extractors/sw_graphic_extractor.py
@@ -12,18 +12,19 @@ def sw_graphic_extractor(classification):
     frame = 'frame0'
     if len(classification['annotations']) > 0:
         annotation = classification['annotations'][0]
-        for value in annotation['value']:
-            if ('type' in value) and ((value['type'] == 'graphic') or (value['type'] == 'image')):
-                exist = exist_and_finite(value, 'x') and exist_and_finite(value, 'y') and exist_and_finite(value, 'width') and exist_and_finite(value, 'height')
-                if (value['type'] == 'graphic'):
-                    exist = exist and exist_and_finite(value, 'tag')
+        if isinstance(annotation['value'], list):
+            for value in annotation['value']:
+                if ('type' in value) and ((value['type'] == 'graphic') or (value['type'] == 'image')):
+                    exist = exist_and_finite(value, 'x') and exist_and_finite(value, 'y') and exist_and_finite(value, 'width') and exist_and_finite(value, 'height')
+                    if (value['type'] == 'graphic'):
+                        exist = exist and exist_and_finite(value, 'tag')
+                        if exist:
+                            extract.setdefault(frame, OrderedDict())
+                            extract[frame].setdefault('tool0_tag', []).append(value['tag'])
                     if exist:
                         extract.setdefault(frame, OrderedDict())
-                        extract[frame].setdefault('tool0_tag', []).append(value['tag'])
-                if exist:
-                    extract.setdefault(frame, OrderedDict())
-                    extract[frame].setdefault('tool0_x', []).append(float(value['x']))
-                    extract[frame].setdefault('tool0_y', []).append(float(value['y']))
-                    extract[frame].setdefault('tool0_width', []).append(float(value['width']))
-                    extract[frame].setdefault('tool0_height', []).append(float(value['height']))
+                        extract[frame].setdefault('tool0_x', []).append(float(value['x']))
+                        extract[frame].setdefault('tool0_y', []).append(float(value['y']))
+                        extract[frame].setdefault('tool0_width', []).append(float(value['width']))
+                        extract[frame].setdefault('tool0_height', []).append(float(value['height']))
     return extract
