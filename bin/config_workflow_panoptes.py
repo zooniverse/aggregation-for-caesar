0e1b0952223407a44896002efc50b4d2ee3590a5
CKrawczyk <coleman.krawczyk@gmail.com>
2018-08-08 15:01:41 +0100

    Add ability to extract task/tool/answer labels from workflow contents
    This method will replace the `--human` command line switch that was 
    previously in place.  This will ensure that column names are short.

--- bin/config_workflow_panoptes.py
+++ bin/config_workflow_panoptes.py
@@ -22,22 +22,26 @@ standard_reducers = {
 }
 
 
-def config_workflow(workflow_csv, workflow_id, version=None, keywords={}):
-    if not isinstance(workflow_csv, io.IOBase):
-        workflow_csv = open(workflow_csv, 'r', encoding='utf-8')
+def get_file_instance(file):
+    if not isinstance(file, io.IOBase):
+        file = open(file, 'r', encoding='utf-8')
+    return file
 
+
+def config_workflow(workflow_csv, workflow_id, version=None, keywords={}, workflow_content=None, minor_version=None):
+    workflow_csv = get_file_instance(workflow_csv)
     with workflow_csv as workflow_csv_in:
         workflows = pandas.read_csv(workflow_csv_in, encoding='utf-8')
 
     if version is None:
         version = workflows[workflows.workflow_id == workflow_id].version.max()
-        warnings.warn('No workflow version was specified, defaulting to version {0}'.format(version))
+        warnings.warn('No major workflow version was specified, defaulting to version {0}'.format(version))
 
     wdx = (workflows.workflow_id == workflow_id) & (workflows.version == version)
     if wdx.sum() == 0:
-        raise IndexError('workflow ID and workflow version combination does not exist')
+        raise IndexError('workflow ID and workflow major version combination does not exist')
     if wdx.sum() > 1:
-        raise IndexError('workflow ID and workflow version combination is not unique')
+        raise IndexError('workflow ID and workflow major version combination is not unique')
     workflow = workflows[wdx].iloc[0]
     workflow_tasks = json.loads(workflow.tasks)
     extractor_config = extractors.workflow_extractor_config(workflow_tasks, keywords=keywords)
@@ -49,15 +53,36 @@ def config_workflow(workflow_csv, workflow_id, version=None, keywords={}):
     filename = 'Extractor_config_workflow_{0}_V{1}.yaml'.format(workflow_id, version)
     with open(filename, 'w', encoding='utf-8') as stream:
         yaml.dump(config, stream=stream, default_flow_style=False)
+    task_set = set({})
     for extractor in extractor_config.keys():
         reducer_config = {
             'reducer_config': {
                 standard_reducers[extractor]: {}
             }
         }
+        for task in extractor_config[extractor]:
+            task_set.add(task['task'])
         filename = 'Reducer_config_workflow_{0}_V{1}_{2}.yaml'.format(workflow_id, version, extractor)
         with open(filename, 'w', encoding='utf-8') as stream:
             yaml.dump(reducer_config, stream=stream, default_flow_style=False)
+    if workflow_content is not None:
+        workflow_content = get_file_instance(workflow_content)
+        with workflow_content as workflow_content_in:
+            contents = pandas.read_csv(workflow_content_in, encoding='utf-8')
+        if minor_version is None:
+            minor_version = contents[contents.workflow_id == workflow_id].version.max()
+            warnings.warn('No minor workflow version was specified, defaulting to version {0}'.format(minor_version))
+        cdx = (contents.workflow_id == workflow_id) & (contents.version == minor_version)
+        if cdx.sum() == 0:
+            raise IndexError('workflow ID and workflow minor version combination does not exist')
+        if cdx.sum() > 1:
+            raise IndexError('workflow ID and workflow mainor version combination is not unique')
+        content = contents[cdx].iloc[0]
+        strings = eval(content.strings)
+        stirngs_extract = {key: value for key, value in strings.items() for task in task_set if (key.startswith(task) and ('help' not in key))}
+        filename = 'Task_labels_workflow_{0}_V{1}.{2}.yaml'.format(workflow_id, version, minor_version)
+        with open(filename, 'w', encoding='utf-8') as stream:
+            yaml.dump(stirngs_extract, stream=stream, default_flow_style=False)
     return config
 
 
@@ -79,7 +104,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-v",
         "--version",
-        help="the workflow version to extract",
+        help="The major workflow version to extract",
         type=int
     )
     parser.add_argument(
@@ -89,11 +114,25 @@ if __name__ == "__main__":
         type=json.loads,
         default={}
     )
+    parser.add_argument(
+        "-c",
+        "--workflow_content",
+        help="The (optional) workflow content file can be provided to create a lookup table for task/answer/tool numbers to the text used on the workflow.",
+        type=argparse.FileType('r', encoding='utf-8')
+    )
+    parser.add_argument(
+        "-m",
+        "--minor_version",
+        help="The minor worikow version used to create the lookup table for the workflow content",
+        type=int
+    )
     args = parser.parse_args()
 
     config_workflow(
         args.workflow_csv,
         args.workflow_id,
         version=args.version,
-        keywords=args.keywords
+        keywords=args.keywords,
+        workflow_content=args.workflow_content,
+        minor_version=args.minor_version
     )
