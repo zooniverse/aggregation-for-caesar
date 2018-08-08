ee2dcc677e0ea2e60d0769739b168b20b59569f9
CKrawczyk <coleman.krawczyk@gmail.com>
2018-08-08 15:02:38 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='panoptes_aggregation',
-    version='0.1',
+    version='1.0',
     description='Aggegation code for Zooniverse panoptes projects.',
     classifiers=['Programming Language :: Python :: 3 :: Only'],
     url='https://github.com/zooniverse/aggregation-for-caesar',
@@ -10,7 +10,11 @@ setup(
     author_email='coleman@zooniverse.org',
     test_suite='nose.collector',
     tests_require=['nose'],
-    scripts=['bin/extract_panoptes_csv.py', 'bin/reduce_panoptes_csv.py'],
+    scripts=[
+        'bin/extract_panoptes_csv.py',
+        'bin/reduce_panoptes_csv.py',
+        'bin/config_workflow_panoptes'
+    ],
     packages=find_packages(),
     include_package_data=True,
     extra_require={
