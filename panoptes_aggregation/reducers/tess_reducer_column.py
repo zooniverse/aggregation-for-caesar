9937c8eb1d0a15c6244742094258eb454671d570
CKrawczyk <coleman.krawczyk@gmail.com>
2019-02-14 13:59:35 +0000

    Update column reducer to use new clustering method
    The reducer and test have been updated to reflect the new data 
    aggregation method used by TESS.  `user_id` and `relevant_reduction` are 
    passed in so the reducer can report what users identified each cluster 
    and calculate weighted counts for each cluster.

--- panoptes_aggregation/reducers/tess_reducer_column.py
+++ panoptes_aggregation/reducers/tess_reducer_column.py
@@ -1,21 +1,19 @@
 from .reducer_wrapper import reducer_wrapper
-from .subtask_reducer_wrapper import subtask_wrapper
 from ..shape_tools import SHAPE_LUT
 from collections import OrderedDict
-from scipy.stats import norm
+from sklearn.cluster import DBSCAN
 import numpy as np
-import peakutils
 
 DEFAULTS = {
-    'x_min': {'default': 0, 'type': float},
-    'x_max': {'default': 1200, 'type': float},
-    'x_step': {'default': 0.1, 'type': float},
-    'thres': {'default': 0.05, 'type': float},
-    'min_dist': {'default': 2, 'type': float}
+    'eps': {'default': 5.0, 'type': float},
+    'min_samples': {'default': 3, 'type': int},
+    'algorithm': {'default': 'auto', 'type': str},
+    'leaf_size': {'default': 30, 'type': int},
+    'p': {'default': None, 'type': float}
 }
 
 
-def process_data(data):
+def process_data(data, **kwargs_extra_data):
     '''Process a list of extractions into lists of `x` and `y` sorted by `tool`
 
     Parameters
@@ -26,59 +24,50 @@ def process_data(data):
 
     Returns
     -------
-    processed_data : dict
-        A dictionary with each key being a `tool` with a list of (`x`, `y`, ...)
-        tuples as a vlaue. Each shape parameter shows up in this list.
+    processed_data : np.array
+        An Nx2 numpy array containing the *center* and width of each column drawn
     '''
     shape_params = SHAPE_LUT['column']
     unique_frames = set(sum([list(d.keys()) for d in data], []))
-    data_by_tool = {
-        'count_classified': len(data),
-    }
+    data_by_tool = []
+    index_by_tool = []
     for frame in unique_frames:
-        data_by_tool[frame] = {}
         unique_tools = set(sum([['_'.join(k.split('_')[:-1]) for k in d.get(frame, {}).keys()] for d in data], []))
         for tool in unique_tools:
-            data_by_tool[frame].setdefault('{0}_count'.format(tool), 0)
-            for d in data:
+            for ddx, d in enumerate(data):
                 if frame in d:
-                    data_by_tool[frame]['{0}_count'.format(tool)] += 1
-                    data_by_tool[frame].setdefault(tool, [])
                     keys = ['{0}_{1}'.format(tool, param) for param in shape_params]
                     if np.all([k in d[frame] for k in keys]):
                         params_list = list(zip(*(d[frame][k] for k in keys)))
-                        data_by_tool[frame][tool] += params_list
-    return data_by_tool
+                        index_by_tool += [ddx] * len(params_list)
+                        data_by_tool += params_list
+    return {'data': data_by_tool, 'index': index_by_tool}
 
 
-@reducer_wrapper(process_data=process_data, defaults_data=DEFAULTS)
-@subtask_wrapper
-def tess_reducer_column(
-    data_by_tool,
-    x_min=DEFAULTS['x_min']['default'],
-    x_max=DEFAULTS['x_max']['default'],
-    x_step=DEFAULTS['x_step']['default'],
-    thres=DEFAULTS['thres']['default'],
-    min_dist=DEFAULTS['min_dist']['default'],
-):
-    count_classified = data_by_tool.pop('count_classified')
-    x_eval = np.arange(x_min, x_max + x_step, x_step)
+@reducer_wrapper(process_data=process_data, defaults_data=DEFAULTS, user_id=True, relevant_reduction=True)
+def tess_reducer_column(data_by_tool, **kwargs):
+    user_id = np.array(kwargs.pop('user_id'))
+    relevant_reduction = kwargs.pop('relevant_reduction')
+    skill = np.array([rr['skill'] for rr in relevant_reduction])
     clusters = OrderedDict()
-    for frame, frame_data in data_by_tool.items():
-        clusters[frame] = OrderedDict()
-        for tool, tool_data in frame_data.items():
-            if tool.endswith('_count'):
-                clusters[frame]['{0}_ratio'.format(tool)] = tool_data / (count_classified - tool_data)
-            else:
-                tick_pdf = np.zeros_like(x_eval)
-                for column in tool_data:
-                    x = column[0] + (0.5 * column[1])  # center the column
-                    width = column[1]
-                    tick_pdf += norm.pdf(x_eval, loc=x, scale=width/2.355)  # divide the width by two to use the FWHM instead of the standdard deviation
-                peak_indexes = peakutils.indexes(tick_pdf, thres=thres, min_dist=min_dist)
-                if len(peak_indexes) > 0:
-                    clusters[frame]['{0}_peak_x'.format(tool)] = x_eval[peak_indexes].round(5).tolist()
-                    clusters[frame]['{0}_peak_pdf'.format(tool)] = tick_pdf[peak_indexes].round(5).tolist()
-                    clusters[frame]['{0}_pdf'.format(tool)] = tick_pdf.round(5).tolist()
-                    clusters[frame]['{0}_x_eval'.format(tool)] = x_eval.round(5).tolist()
+    loc = np.array(data_by_tool['data'])
+    index = np.array(data_by_tool['index'])
+    loc[:, 0] += 0.5 * loc[:, 1]
+    if loc.shape[0] >= kwargs['min_samples']:
+        db = DBSCAN(**kwargs).fit(loc)
+        for k in set(db.labels_):
+            if k > -1:
+                idx = db.labels_ == k
+                # center and width of the cluster
+                k_loc = loc[idx].mean(axis=0)
+                clusters.setdefault('centers', []).append(float(k_loc[0]))
+                clusters.setdefault('widths', []).append(float(k_loc[1]))
+                # number of points in the cluster
+                clusters.setdefault('counts', []).append(int(idx.sum()))
+                # weighted number of points in the cluster
+                k_skill = [skill[j] for j in index[idx]]
+                clusters.setdefault('weighted_counts', []).append(float(sum(k_skill)))
+                # user_ids in the cluster
+                k_users = [user_id[j] for j in index[idx]]
+                clusters.setdefault('user_ids', []).append(k_users)
     return clusters
--- panoptes_aggregation/tests/reducer_tests/test_tess_reducer_column.py
+++ panoptes_aggregation/tests/reducer_tests/test_tess_reducer_column.py
@@ -1,8 +1,5 @@
 from panoptes_aggregation.reducers.tess_reducer_column import process_data, tess_reducer_column
 from .base_test_class import ReducerTest
-import numpy as np
-from scipy.stats import norm
-
 
 extracted_data = [
     {
@@ -13,61 +10,68 @@ extracted_data = [
     },
     {
         'frame0': {
-            'T0_tool0_x': [0.0, 100.0],
-            'T0_tool0_width': [1.0, 100.0],
+            'T0_tool0_x': [0.0, 100.0, 55.0],
+            'T0_tool0_width': [1.0, 100.0, 4.0],
         }
     },
     {
         'frame0': {
-            'T0_tool0_x': [0.0, 100.0, 55.0],
-            'T0_tool0_width': [100.0, 5.0, 4.0]
+            'T0_tool0_x': [0.0, 100.0],
+            'T0_tool0_width': [100.0, 5.0]
         }
     },
     {},
     {}
 ]
 
-processed_data = {
-    'count_classified': 5,
-    'frame0': {
-        'T0_tool0_count': 3,
-        'T0_tool0': [
-            (0.0, 1.0),
-            (100.0, 100.0),
-            (50.0, 50.0),
-            (0.0, 1.0),
-            (100.0, 100.0),
-            (0.0, 100.0),
-            (100.0, 5.0),
-            (55.0, 4.0)
-        ]
-    }
+kwargs_extra_data = {
+    'user_id': [
+        1,
+        2,
+        3,
+        4,
+        5
+    ],
+    'relevant_reduction': [
+        {'skill': 0.4},
+        {'skill': 0.8},
+        {'skill': 0.1},
+        {'skill': 0.3},
+        {'skill': 0.2}
+    ]
 }
 
-x_eval = np.arange(0, 150.01, 0.01)
-x_center = [0.5, 150, 75, 0.5, 150, 50, 102.5, 57]
-width = [1, 100, 50, 1, 100, 100, 5, 4]
-tick_pdf = np.zeros_like(x_eval)
-for x, w in zip(x_center, width):
-    tick_pdf += norm.pdf(x_eval, loc=x, scale=w/2.355)
+processed_data = {
+    'data': [
+        (0.0, 1.0),
+        (100.0, 100.0),
+        (50.0, 50.0),
+        (0.0, 1.0),
+        (100.0, 100.0),
+        (55.0, 4.0),
+        (0.0, 100.0),
+        (100.0, 5.0)
+    ],
+    'index': [
+        0,
+        0,
+        0,
+        1,
+        1,
+        1,
+        2,
+        2
+    ]
+}
 
 reduced_data = {
-    'frame0': {
-        'T0_tool0_peak_x': [
-            0.5,
-            57.01,
-            102.49
-        ],
-        'T0_tool0_peak_pdf': [
-            1.88386,
-            0.25897,
-            0.21045
-        ],
-        'T0_tool0_pdf': tick_pdf.round(5).tolist(),
-        'T0_tool0_x_eval': x_eval.round(5).tolist(),
-        'T0_tool0_count_ratio': 1.5
-    }
+    'centers': [0.5, 150.0, 78.16666666666667],
+    'widths': [1.0, 100.0, 19.666666666666668],
+    'counts': [2, 2, 3],
+    'weighted_counts': [1.2000000000000002, 1.2000000000000002, 1.3000000000000003],
+    'user_ids': [[1, 2], [1, 2], [1, 2, 3]]
 }
+
 
 TestShapeReducerColumn = ReducerTest(
     tess_reducer_column,
@@ -77,10 +81,8 @@ TestShapeReducerColumn = ReducerTest(
     reduced_data,
     'Test TESS column reducer',
     kwargs={
-        'x_min': 0,
-        'x_max': 150,
-        'x_step': 0.01,
-        'thres': 0.05,
-        'min_dist': 2
-    }
+        'eps': 50,
+        'min_samples': 2
+    },
+    network_kwargs=kwargs_extra_data
 )
