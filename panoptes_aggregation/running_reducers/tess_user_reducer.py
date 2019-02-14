594c93823e34658aa3de27a49b5107bc6bbfe986
CKrawczyk <coleman.krawczyk@gmail.com>
2019-02-14 17:23:09 +0000

    Add base code for the TESS user reducer
    This reducer will calculate the user's skill based on the dificulty of 
    the subeject (passed in on `relevant_reduction`) and how many GS 
    transits they found (passed in on `data`).  This is a running reducer so 
    a `store` is used to keep track of the running `seed` value and the 
    number of GS subjects seen (`count`).
    
    For this template I made some reasonable assumptions for the shape of 
    the data being passed in and what needs to be returned.

---
+++ panoptes_aggregation/running_reducers/tess_user_reducer.py
@@ -0,0 +1,30 @@
+from runnign_reducer_wrapper import running_reducer_wrapper
+import numpy as np
+
+
+@running_reducer_wrapper(relevant_reduction=True)
+def tess_user_reducer(data, store, **kwargs):
+    relevant_reduction = kwargs.pop('relevant_reduction')
+    # not sure what data the stats reducer will return
+    # this assuems a dict {'True': 3, 'False': 5}
+    number_correct = relevant_reduction['True']
+    number_incorrect = relevant_reduction['False']
+    d_subject = number_correct / (number_correct + number_incorrect)
+    # not sure what shape data will be in the end
+    # this assuems it is a list of `True` and `False` (or 0 and 1)
+    user_correct = sum(data)
+    user_incorrect = len(data) - user_correct
+    seed_current = (2 * user_correct - user_incorrect) * d_subject
+    seed = store.get('seed', 0) + seed_current
+    count = store.get('count', 0) + 1
+    store = {
+        'seed': seed,
+        'count': count
+    }
+    c0 = 1
+    skill = c0 * pow((1.0 + np.log10(count)), (float(seed)/float(count)))
+    skill = min([3.0, max([0.05, skill])])
+    return {
+        'data': {'skill': skill},
+        'store': store
+    }
