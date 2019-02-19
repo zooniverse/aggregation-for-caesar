35540d0c3fc1d4b1ec94c3b600d4cb6254c02a98
Campbell Allen <campbell.allen@gmail.com>
2019-02-19 15:03:44 -0600

    lock the numpy version
    fix a numpy dependency error

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
         'collatex==2.1.2',
         'hdbscan',
         'lxml',
-        'numpy>=1.14.5',
+        'numpy==1.15.0',
         'nose',
         'pandas',
         'progressbar2',
