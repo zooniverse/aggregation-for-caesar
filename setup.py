79e4f89d4e1e86adb88709ac34685607b04f57be
Campbell Allen <campbell.allen@gmail.com>
2019-03-13 14:51:30 +0000

    Update numpy dep setup.py

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
         'collatex==2.1.2',
         'hdbscan',
         'lxml',
-        'numpy==1.15.0',
+        'numpy==1.16.2,
         'nose',
         'pandas',
         'progressbar2',
