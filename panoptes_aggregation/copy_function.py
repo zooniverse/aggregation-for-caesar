4bac7e2c21e3bde265306b19118ea1a4e0f482cd
CKrawczyk <coleman.krawczyk@gmail.com>
2019-03-21 16:38:44 +0000

    Update copy_function doc string
    Citing the source of the function.

--- panoptes_aggregation/copy_function.py
+++ panoptes_aggregation/copy_function.py
@@ -3,7 +3,8 @@ import functools
 
 
 def copy_function(f, new_name):
-    """Based on http://stackoverflow.com/a/6528148/190597 (Glenn Maynard)"""
+    '''Based on http://stackoverflow.com/a/6528148/190597 (Glenn Maynard) and
+    https://stackoverflow.com/a/13503277/1052418 (unutbu)'''
     g = types.FunctionType(f.__code__, f.__globals__, name=f.__name__,
                            argdefs=f.__defaults__,
                            closure=f.__closure__)
