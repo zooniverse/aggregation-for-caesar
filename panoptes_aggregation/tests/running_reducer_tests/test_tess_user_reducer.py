5949159fbecf08319f70021104f50d9cb8029317
CKrawczyk <coleman.krawczyk@gmail.com>
2019-04-09 10:25:49 +0100

    Update TESS user reducer to the case of no GS extract
    Add test coverage for the no relevant reduction and no GS extracts 
    cases.  The relevant reduction check has been moved to a `try` block so 
    a value of `None` or a missing `key` will both be handled correctly.

--- panoptes_aggregation/tests/running_reducer_tests/test_tess_user_reducer.py
+++ panoptes_aggregation/tests/running_reducer_tests/test_tess_user_reducer.py
@@ -45,3 +45,43 @@ TestTESSUserReduer = RunningReducerTestNoProcessing(
     'Test TESS User reducer',
     network_kwargs=kwargs_extra_data
 )
+
+kwargs_extra_data_no_rr = {
+    'relevant_reduction': [
+        None
+    ],
+    'store': {}
+}
+
+reduced_data_no_rr = {
+    'skill': 1.0,
+    '_store': {
+        'seed': 0,
+        'count': 4
+    }
+}
+
+TestTESSUserReduerNoRR = RunningReducerTestNoProcessing(
+    tess_user_reducer,
+    extracted_data,
+    reduced_data_no_rr,
+    'Test TESS User reducer with no relevant reduction',
+    network_kwargs=kwargs_extra_data_no_rr
+)
+
+kwargs_extra_data_no_gs_extract = {
+    'relevant_reduction': [
+        {
+            'data': {}
+        }
+    ],
+    'store': {}
+}
+
+TestTESSUserReduerNoGSExtract = RunningReducerTestNoProcessing(
+    tess_user_reducer,
+    extracted_data,
+    reduced_data_no_rr,
+    'Test TESS User reducer with no GS extract',
+    network_kwargs=kwargs_extra_data_no_gs_extract
+)
