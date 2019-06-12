68ee11ed66b6a11dd3e095dc297c5ecba21632d4
CKrawczyk <coleman.krawczyk@gmail.com>
2019-06-12 17:01:42 +0100

    Add optics text reducer utility functions
    These are functions that will help the optics text reducer along with 
    tests.

---
+++ panoptes_aggregation/reducers/optics_text_utils.py
@@ -0,0 +1,163 @@
+'''
+Utilities for `optics_line_text_reducer`
+----------------------------------------
+This module provides utilities used to reduce the polygon-text extractions
+for :mod:`panoptes_aggregation.reducers.optics_line_text_reducer`.  It
+assumes that all extracts are full lines of text in the document.
+'''
+import numpy as np
+import Levenshtein
+import collections
+import copy
+import re
+
+
+def strip_tags(s):
+    '''Remove square bracket tags from text and consolidating whitespace
+
+    Parameters
+    ----------
+    s : string
+        The input string
+
+    Returns
+    -------
+    clean_s : string
+        The cleaned string
+    '''
+    no_brackets = re.sub('[\[].*?[\]]', '', s)  # noqa: W605
+    unify_space = ' '.join(no_brackets.split())
+    return unify_space
+
+
+def metric(a, b, data_in=[]):
+    '''Calculate the distance between two drawn lines that have text
+    associated with them.  This distance is found by summing the euclidean
+    distance between the start points of each line, the euclidean distance
+    between the end poitns of each line, and the Levenshtein distance
+    of the text for each line.  The Levenshtein distance is done after
+    stripping text tags and consolidating whitespace.
+
+    To use this metric within the clustering code without haveing to
+    precompute the full distance matrix `a` and `b` are index mappings to
+    the data contained in `data_in`.  `a` and `b` also contain the user
+    information that is used to help prevent self-clustering.
+
+    Parameters
+    ----------
+    a : list
+        A two element list containing [index mapping to data, index mapping to user]
+    b : list
+        A two element list containing [index mapping to data, index mapping to user]
+    data_in : list
+        A list of dicts that take the form
+        {`x`: [start_x, end_x], `y`: [start_y, end_y], 'text': ['text for line']}
+        There is one element in this list for each classification made.
+
+    Returns
+    -------
+    distance: float
+        The distance between `a` and `b`
+    '''
+    if a[0] == b[0]:
+        # The same data point, the distance is zero
+        return 0
+    if a[1] == b[1]:
+        # The same users, distance is inf
+        return np.inf
+    data_a = data_in[int(a[0])]
+    data_b = data_in[int(b[0])]
+    dx = (np.array(data_a['x']) - np.array(data_b['x']))**2
+    dy = (np.array(data_a['y']) - np.array(data_b['y']))**2
+    dt = Levenshtein.distance(
+        strip_tags(data_a['text'][0]),
+        strip_tags(data_b['text'][0])
+    )
+    return np.sqrt(dx + dy).sum() + dt
+
+
+def get_min_samples(N):
+    '''Get the `min_samples` attribute based on the number of
+    users who have transcribed the subject.  These values were
+    found based on example data from ASM.
+
+    Parameters
+    ----------
+    N : integer
+        The number of users who have see the subject
+
+    Returns
+    -------
+    min_samples : integer
+        The value to use for the min_samples keyword in OPTICS
+    '''
+    if N <= 6:
+        return 2
+    elif N <= 10:
+        return 3
+    elif N <= 15:
+        return 4
+    elif N <= 20:
+        return 5
+    else:
+        return int(0.25 * N)
+
+
+def remove_user_duplication(labels_, core_distances_, users):
+    '''Make sure a users only shows up in a cluster at most once.
+    If a user does show up more than once in a cluster take the point
+    with the smallest core distance, all others are assigned as noise (-1).
+
+    Parameters
+    ----------
+    labels_ : numpy.array
+        A list containing the cluster labels for each data point
+    core_distances_ : numpy.array
+        A list of core distance for each data point
+    users : numpy.array
+        A list of indices that map to users, one for each data point
+
+    Returns
+    -------
+    clean_labels_ : numpy.array
+        A list containing the new cluster labels.
+    '''
+    clean_labels = copy.deepcopy(labels_)
+    unique_labels = np.unique(labels_)
+    gdx = unique_labels > -1
+    for l in unique_labels[gdx]:
+        cdx = labels_ == l
+        user_counts = collections.Counter(users[cdx]).most_common()
+        if user_counts[0][1] > 1:
+            clean_labels_cdx = clean_labels[cdx]
+            for user_count in user_counts:
+                udx = users[cdx] == user_count[0]
+                clean_labels_cdx_udx = clean_labels_cdx[udx]
+                if user_count[1] > 1:
+                    min_idx = core_distances_[cdx][udx].argmin()
+                    mask = np.ones(udx.sum(), dtype=bool)
+                    mask[min_idx] = False
+                    clean_labels_cdx_udx[mask] = -1
+                else:
+                    break
+                clean_labels_cdx[udx] = clean_labels_cdx_udx
+            clean_labels[cdx] = clean_labels_cdx
+    return clean_labels
+
+
+# def cluster_data(X, data, min_samples='auto'):
+#     if min_samples == 'auto':
+#         num_users = len(np.unique(X[:, 1]))
+#         min_samples = get_min_samples(num_users)
+#     db = OPTICS(
+#         min_samples=min_samples,
+#         metric=metric,
+#         metric_params={'data_in': data}
+#     )
+#     db.fit(X)
+#     clean_labels = remove_user_duplication(
+#         db.labels_,
+#         db.core_distances_,
+#         X[:, 1]
+#     )
+#     return db, clean_labels
---
+++ panoptes_aggregation/tests/utility_tests/test_optics_text_utils.py
@@ -0,0 +1,89 @@
+import unittest
+import numpy as np
+from panoptes_aggregation.reducers import optics_text_utils
+
+data = [
+    {
+        'x': [1, 5],
+        'y': [1, 1],
+        'text': ['This is   some [underline]test  text[/underline]']
+    },
+    {
+        'x': [1, 3],
+        'y': [1, 1],
+        'text': ['This is some test text']
+    },
+    {
+        'x': [1, 5],
+        'y': [1, 3],
+        'text': ['This is some test text']
+    },
+    {
+        'x': [1, 5],
+        'y': [1, 1],
+        'text': ['This is some test test']
+    },
+]
+
+
+class TextOpticsTextUtils(unittest.TestCase):
+    def test_strip_tags(self):
+        '''Test strip tags'''
+        s = 'This is   some [underline]test  text[/underline]'
+        expected = 'This is some test text'
+        result = optics_text_utils.strip_tags(s)
+        self.assertEqual(result, expected)
+
+    def test_metric_same_point(self):
+        '''Test metric with same point'''
+        result = optics_text_utils.metric([0, 0], [0, 0], data_in=data)
+        self.assertEqual(result, 0)
+
+    def test_metric_same_user(self):
+        '''Test metric with same point'''
+        result = optics_text_utils.metric([0, 0], [1, 0], data_in=data)
+        self.assertEqual(result, np.inf)
+
+    def test_metric_distances(self):
+        '''Test metric distances'''
+        pair_list = [
+            [[0, 0], [1, 1]],
+            [[0, 0], [2, 1]],
+            [[0, 0], [3, 1]]
+        ]
+        expected_distances = [
+            2,
+            2,
+            1
+        ]
+        for i in range(len(pair_list)):
+            with self.subTest(i=i):
+                a, b = pair_list[i]
+                result = optics_text_utils.metric(a, b, data_in=data)
+                self.assertEqual(result, expected_distances[i])
+
+    def test_get_min_samples(self):
+        '''Test auto values for min_samples'''
+        number_users = [2, 7, 11, 16, 21, 24, 40]
+        min_samples = [2, 3, 4, 5, 5, 6, 10]
+        for i in range(len(number_users)):
+            with self.subTest(i=i):
+                result = optics_text_utils.get_min_samples(number_users[i])
+                self.assertEqual(result, min_samples[i])
+
+    def test_remove_user_duplication(self):
+        '''Test removing duplicate users within a cluster'''
+        labels = np.array([0, 0, 1, 1, 1, 2, 2])
+        core_distances = np.array([0.5, 0, 0, 1, 2, 0, 0.5])
+        users = np.array([0, 0, 0, 1, 1, 0, 1])
+        expected = np.array([-1, 0, 1, 1, -1, 2, 2])
+        result = optics_text_utils.remove_user_duplication(
+            labels,
+            core_distances,
+            users
+        )
+        np.testing.assert_array_equal(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
