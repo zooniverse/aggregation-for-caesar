6386ee02bccdcba52d905c0aca5b1439301b847a
CKrawczyk <coleman.krawczyk@gmail.com>
2019-07-29 11:52:55 +0100

    Add text reducer
    This reducer takes the text extracts and creates an alignment table 
    useing collatex.  Blank extracts (white space only) are removed before 
    the alignment is done.

--- panoptes_aggregation/reducers/__init__.py
+++ panoptes_aggregation/reducers/__init__.py
@@ -13,6 +13,7 @@ from .shape_reducer_hdbscan import shape_reducer_hdbscan
 from .slider_reducer import slider_reducer
 from .tess_reducer_column import tess_reducer_column
 from .tess_gold_standard_reducer import tess_gold_standard_reducer
+from .text_reducer import text_reducer
 from ..copy_function import copy_function
 
 shortcut_reducer = copy_function(question_reducer, 'shortcut_reducer')
@@ -32,5 +33,6 @@ reducers = {
     'shape_reducer_hdbscan': shape_reducer_hdbscan,
     'slider_reducer': slider_reducer,
     'tess_reducer_column': tess_reducer_column,
-    'tess_gold_standard_reducer': tess_gold_standard_reducer
+    'tess_gold_standard_reducer': tess_gold_standard_reducer,
+    'text_reducer': text_reducer
 }
---
+++ panoptes_aggregation/reducers/text_reducer.py
@@ -0,0 +1,63 @@
+'''
+Text Tool Reducer
+-----------------
+This module provides functions to reducer the panoptes text tool into an
+alignment table.
+'''
+import collatex as col
+from .text_utils import consensus_score, tokenize
+from .reducer_wrapper import reducer_wrapper
+
+# override the built-in tokenize
+col.core_classes.WordPunctuationTokenizer.tokenize = tokenize
+
+
+def process_data(data_list):
+    '''Flatten list of extracts into a list of strings.  Empty strings
+    are not returned'''
+    return [d['text'] for d in data_list if d['text'].strip() != '']
+
+
+@reducer_wrapper(process_data=process_data)
+def text_reducer(data, **kwargs):
+    '''Reduce a list of text into an alignment table
+    Parameters
+    ----------
+    data : list
+        A list of strigs to be aligned
+
+    Returns
+    -------
+    reduction : dict
+        A dictionary with the following keys:
+
+        * `aligned_text` : A list of lists containing the aligned text.
+            There is one list for each identified word, and each of those lists contains
+            one item for each user that entered text. If the user did not transcribe
+            a word an empty string is used.
+        * `number_views` : Number of volunteers who entered non-blank text
+        * `consensus_score` : The average number of users who's text agreed
+            Note, if `consensus_score` is the same a `number_views` every user agreed with each other
+    '''
+    reduction = {}
+    if len(data) > 0:
+        witness_keys = []
+        aligned_text = []
+        collation = col.Collation()
+        for index, text in enumerate(data):
+            key = str(index)
+            witness_keys.append(key)
+            collation.add_plain_witness(key, text)
+        alignment_table = col.collate(collation, near_match=True, segmentation=False)
+        for cols in alignment_table.columns:
+            word_dict = cols.tokens_per_witness
+            word_list = []
+            for key in witness_keys:
+                word_list.append(str(word_dict.get(key, [''])[0]))
+            aligned_text.append(word_list)
+        reduction = {
+            'aligned_text': aligned_text,
+            'number_views': len(data),
+            'consensus_score': consensus_score(aligned_text)
+        }
+    return reduction
