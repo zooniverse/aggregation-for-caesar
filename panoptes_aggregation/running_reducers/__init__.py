5d863581454b776f4cbea4216114815a0ae0b4fd
Coleman Krawczyk <coleman.krawczyk@gmail.com>
2019-08-16 12:08:35 -0500

    Update __init__.py
    fix typo made during merge conflict

--- panoptes_aggregation/running_reducers/__init__.py
+++ panoptes_aggregation/running_reducers/__init__.py
@@ -6,6 +6,6 @@ from .tess_gold_standard_reducer import tess_gold_standard_reducer_rr
 
 running_reducers = {
     'tess_user_reducer': tess_user_reducer,
-    'tess_reducer_column': tess_reducer_column_rr
+    'tess_reducer_column': tess_reducer_column_rr,
     'tess_gold_standard_reducer': tess_gold_standard_reducer_rr
 }
