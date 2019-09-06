651c9e8b795ab45d1cea840fcb79ac45eaa0b2fa
CKrawczyk <coleman.krawczyk@gmail.com>
2019-09-06 15:41:21 +0100

    Optimize question reducer
    The question reducer used `Counter` as an intermediate container for the data. It turns out that summing these `Counter`s is quite slow.
    
    To speed up the reducer a list is made of all answers given (across all extracts) and `Counter` is called at the end to get a count for all unique answers.  This new code is 60% to 85% faster than the previous method (more speed up for larger extracts).

--- panoptes_aggregation/tests/reducer_tests/test_question_reducer.py
+++ panoptes_aggregation/tests/reducer_tests/test_question_reducer.py
@@ -1,6 +1,5 @@
-from collections import Counter
-from panoptes_aggregation.reducers.question_reducer import process_data, question_reducer
-from .base_test_class import ReducerTest
+from panoptes_aggregation.reducers.question_reducer import question_reducer
+from .base_test_class import ReducerTestNoProcessing
 
 extracted_data = [
     {'a': 1, 'b': 1},
@@ -9,35 +8,18 @@ extracted_data = [
     {'b': 1, 'a': 1}
 ]
 
-processed_data = [
-    Counter({'a': 1, 'b': 1}),
-    Counter({'a': 1}),
-    Counter({'b': 1, 'c': 1}),
-    Counter({'b': 1, 'a': 1})
-]
-
 reduced_data = {
     'a': 3,
     'b': 3,
     'c': 1
 }
 
-TestQuestionReducer = ReducerTest(
+TestQuestionReducer = ReducerTestNoProcessing(
     question_reducer,
-    process_data,
     extracted_data,
-    processed_data,
     reduced_data,
-    'Test question reducer',
-    processed_type='list'
+    'Test question reducer'
 )
-
-processed_data_pairs = [
-    Counter({'a+b': 1}),
-    Counter({'a': 1}),
-    Counter({'b+c': 1}),
-    Counter({'a+b': 1})
-]
 
 reduced_data_pairs = {
     'a+b': 2,
@@ -45,13 +27,10 @@ reduced_data_pairs = {
     'b+c': 1
 }
 
-TestQuestionReducerPairs = ReducerTest(
+TestQuestionReducerPairs = ReducerTestNoProcessing(
     question_reducer,
-    process_data,
     extracted_data,
-    processed_data_pairs,
     reduced_data_pairs,
     'Test question reducer as pairs',
-    pkwargs={'pairs': True},
-    processed_type='list'
+    kwargs={'pairs': True}
 )
