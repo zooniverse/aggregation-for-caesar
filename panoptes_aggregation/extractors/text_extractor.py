93f88c10533d850a96e8af24a2ac1db1ef7ec7a3
CKrawczyk <coleman.krawczyk@gmail.com>
2020-03-17 13:14:27 +0000

    Add ID and gold standard flags to text reducer
    This brings the output in line with the transcription reducers

--- panoptes_aggregation/extractors/text_extractor.py
+++ panoptes_aggregation/extractors/text_extractor.py
@@ -6,23 +6,26 @@ This module provides a function to extract text tasks from panoptes annotations
 from .extractor_wrapper import extractor_wrapper
 
 
-@extractor_wrapper()
-def text_extractor(classifiction, **kwargs):
+@extractor_wrapper(gold_standard=True)
+def text_extractor(classification, gold_standard=False, **kwargs):
     '''
     Extract annotations from a text task as a string.
 
     Parameters
     ----------
-    classifiction : dict
+    classification : dict
         A dictionary containing annotations as a key that is a list of panoptes annotations
 
     Returns
     -------
     extraction : dict
-        A dictionary with one key `text` containing the string for the text entered for the task
+        A dictionary with two keys
+        * `text`: the string for the text entered for the task
+        * `gold_standard`: bool indicated if the classification was made in gold standard mode
     '''
     extract = {}
-    if len(classifiction['annotations']) > 0:
-        annotaiton = classifiction['annotations'][0]
+    if len(classification['annotations']) > 0:
+        annotaiton = classification['annotations'][0]
         extract['text'] = annotaiton['value']
+        extract['gold_standard'] = gold_standard
     return extract
