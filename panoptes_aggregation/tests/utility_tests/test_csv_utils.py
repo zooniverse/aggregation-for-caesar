4d1ad020a6b347bebf6a34d190464660f66a6ec4
CKrawczyk <coleman.krawczyk@gmail.com>
2020-04-30 11:22:47 +0100

    bring test coverage back up to 100% for csv_utils
    Add two more tests to csv_utils to ensure full coverage of the unflattening function.

--- panoptes_aggregation/tests/utility_tests/test_csv_utils.py
+++ panoptes_aggregation/tests/utility_tests/test_csv_utils.py
@@ -30,13 +30,23 @@ expected_unflatten_renest = {'a': 1, 'b': 0, 'c': {'d': 0, 'e': 1}}
 expected_unflatten = {'a': 1, 'b': 0, 'c.d': 0, 'c.e': 1}
 
 flat_data_text = pandas.DataFrame({
-    'classification_id': [1, 2, 3],
-    'user_id': [1, 2, 3],
-    'data.text': ['1', '10', '10-12']
+    'classification_id': [3, 4],
+    'user_id': [3, 4],
+    'data.text': ['10-12', '5 bags [blue]']
 })
-
-flat_row_text = flat_data_text.iloc[2]
+flat_row_text = flat_data_text.iloc[0]
 expected_unflatten_text = {'text': '10-12'}
+
+flat_row_text_2 = flat_data_text.iloc[1]
+expected_unflatten_text_2 = {'text': '5 bags [blue]'}
+
+flat_data_array = pandas.DataFrame({
+    'classification_id': [1],
+    'user_id': [1],
+    'data.array': ['[[137, 592]]']
+})
+flat_row_array = flat_data_array.iloc[0]
+expected_unflatten_array = {'array': [[137, 592]]}
 
 json_data = pandas.DataFrame({
     'classification_id': [1, 2, 3, 4],
@@ -92,6 +102,16 @@ class TestCSVUtils(unittest.TestCase):
         '''Test unflattening of data with numbers as text'''
         result = csv_utils.unflatten_data(flat_row_text)
         self.assertDictEqual(result, expected_unflatten_text)
+
+    def test_unflatten_text_2(self):
+        '''Test unflattening of data with text containing brackets'''
+        result = csv_utils.unflatten_data(flat_row_text_2)
+        self.assertDictEqual(result, expected_unflatten_text_2)
+
+    def test_unflatten_array(self):
+        '''Test unflattening of data with array as a string'''
+        result = csv_utils.unflatten_data(flat_row_array)
+        self.assertDictEqual(result, expected_unflatten_array)
 
     def test_unflatten_data(self):
         '''Test unflattening of data with out the renest keyword'''
