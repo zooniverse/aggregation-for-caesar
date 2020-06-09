97875e964309229fb6b228ea909b52b98fdef0c8
CKrawczyk <coleman.krawczyk@gmail.com>
2020-06-09 13:13:09 +0100

    Pass correct user IDs to correct subtask
    When one volunteer makes multiple marks their user ID needs to be associated with each of those marks and passed along correctly to the subtask reducer.

--- panoptes_aggregation/reducers/subtask_reducer_wrapper.py
+++ panoptes_aggregation/reducers/subtask_reducer_wrapper.py
@@ -1,5 +1,6 @@
 from functools import wraps
 import numpy as np
+from collections import defaultdict
 from panoptes_aggregation import reducers
 from packaging import version
 
@@ -8,7 +9,7 @@ def subtask_wrapper(func):
     @wraps(func)
     def wrapper(data, **kwargs):
         details_functions = kwargs.pop('details', None)
-        user_id = kwargs.pop('user_id', None)
+        user_id = np.array(kwargs.pop('user_id', []))
         # data_in is the list of original extracts
         # data is the `processed` data_in
         data_in = kwargs.pop('data_in', None)
@@ -20,20 +21,25 @@ def subtask_wrapper(func):
                 keys_details = ['{0}_details'.format(k) for k in details_functions.keys()]
                 keys_labels = ['{0}_cluster_labels'.format(k) for k in details_functions.keys()]
                 keys_clusters = ['{0}_clusters_details'.format(k) for k in details_functions.keys()]
-                for extract in data_in:
+                user_ids_per_subtask = defaultdict(lambda: defaultdict(list))
+                for edx, extract in enumerate(data_in):
                     for frame_key, frame in extract.items():
                         for k in keys_details:
                             output[frame_key].setdefault(k, [])
                             if k in frame:
                                 output[frame_key][k] += frame[k]
+                                user_ids_per_subtask[frame_key][k] += [user_id[edx]] * len(frame[k])
                 for frame_key, _frame in output.items():
                     for kd, kl, kc, df in zip(keys_details, keys_labels, keys_clusters, details_functions.keys()):
                         detail_array = np.array(output[frame_key][kd])
+                        user_id_array = np.array(user_ids_per_subtask[frame_key][kd])
                         if kl in output[frame_key]:
                             cluster_labels = np.array(output[frame_key][kl])
                             for label in np.unique(cluster_labels):
                                 if label > -1:
-                                    detail_cluster = detail_array[cluster_labels == label]
+                                    cdx = cluster_labels == label
+                                    detail_cluster = detail_array[cdx]
+                                    user_id_cluster = user_id_array[cdx]
                                     detail_reduced = []
                                     for ddx, detail in enumerate(detail_cluster.T):
                                         if details_functions[df][ddx] is None:
@@ -41,31 +47,36 @@ def subtask_wrapper(func):
                                         else:
                                             reducer = reducers.reducers[details_functions[df][ddx]]
                                             detail_reduced.append(
-                                                reducer(detail, no_version=True, user_id=user_id)
+                                                reducer(detail, no_version=True, user_id=user_id_cluster)
                                             )
                                     output[frame_key].setdefault(kc, []).append(detail_reduced)
             elif all(classifier_versions >= version.parse('2.0')):
                 # classifier version 2.0 and up
-                for extract in data_in:
+                user_ids_per_subtask = defaultdict(lambda: defaultdict(list))
+                for edx, extract in enumerate(data_in):
                     for frame_key, frame in extract.items():
                         for k in details_functions.keys():
                             output[frame_key].setdefault(k, [])
                             if k in frame:
                                 output[frame_key][k] += frame[k]
-                for frame in output.values():
+                                user_ids_per_subtask[frame_key][k] += [user_id[edx]] * len(frame[k])
+                for frame_key, frame in output.items():
                     unique_tools = set(['_'.join(k.split('_')[:2]) for k in frame.keys()])
                     for tool in unique_tools:
                         subtasks = [k for k in details_functions.keys() if k.startswith(tool)]
                         for subtask in subtasks:
                             cluster_labels = np.array(frame['{0}_cluster_labels'.format(tool)])
                             subtask_array = np.array(frame[subtask])
+                            user_id_array = np.array(user_ids_per_subtask[frame_key][subtask])
                             subtask_reduced = []
                             for label in np.unique(cluster_labels):
                                 if label > -1:
-                                    subtask_cluster = subtask_array[cluster_labels == label]
+                                    cdx = cluster_labels == label
+                                    subtask_cluster = subtask_array[cdx]
+                                    user_id_cluster = user_id_array[cdx]
                                     reducer = reducers.reducers[details_functions[subtask]]
                                     subtask_reduced.append(
-                                        reducer(subtask_cluster, no_version=True, user_id=user_id)
+                                        reducer(subtask_cluster, no_version=True, user_id=user_id_cluster)
                                     )
                             frame['{0}_clusters'.format(subtask)] = subtask_reduced
                 output['classifier_version'] = str(classifier_versions.max())
