026164799b1f49bb19db3e625a57edcb61d26317
Hugh Dickinson <hugh.dickinson@open.ac.uk>
2020-06-30 12:49:40 +0100

    Added extractor to identify classifications that have all task reponses empty. Added reducer to detect when the first n extracts are True. Added tests.

---
+++ panoptes_aggregation/extractors/all_tasks_empty_extractor.py
@@ -0,0 +1,31 @@
+"""
+All Tasks Empty Extractor
+-------------------
+Extractor determines whether all task values are empty.
+"""
+from .extractor_wrapper import extractor_wrapper
+import numpy as np
+
+
+@extractor_wrapper()
+def all_tasks_empty_extractor(classification, **kwargs):
+    """Determine whether all task values in a classification are empty.
+
+    Parameters
+    ----------
+    classification : dict
+        A dictionary containing an `annotations` key that is a list of
+        panoptes annotations
+
+    Returns
+    -------
+    extraction : dict
+        `extraction["result"]` is `True` if all task values are `None`. `False`
+        otherwise.
+    """
+    empty_or_absent_value = [
+        task["value"] is None if "value" in task else True
+        for task in classification["annotations"]
+    ]
+
+    return {"result": len(empty_or_absent_value) == 0 or np.all(empty_or_absent_value)}
---
+++ panoptes_aggregation/reducers/first_n_true_reducer.py
@@ -0,0 +1,44 @@
+"""
+First N True Reducer
+----------------
+This module is designed to reduce boolean-valued extracts e.g.
+:mod:`panoptes_aggregation.extractors.all_tasks_empty_extractor`.
+It returns true if and only if the first N extracts are `True`.
+"""
+from collections import Counter
+from .reducer_wrapper import reducer_wrapper
+import numpy as np
+
+DEFAULTS = {"n": {"default": 0, "type": int}}
+
+
+def extractResultKey(extract):
+    if not isinstance(extract, dict):
+        return False
+    return extract["result"] if "result" in extract else False
+
+
+@reducer_wrapper(defaults_data=DEFAULTS)
+def first_n_true_reducer(data_list, n=0, **kwargs):
+    """Reduce a list of boolean values to a single boolean value.
+
+    Parameters
+    ----------
+    data_list : list
+        A list of dicts containing a "result" key which should correspond with a
+        boolean value.
+
+    n: int
+        The first n results in `data_list` must be `True`.
+
+    Returns
+    -------
+    reduction : dict
+        `reduction["result"]` is `True` if the first n results in `data_list`
+        are `True`. Otherwise `False`.
+    """
+    return {
+        "result": n > 0
+        and len(data_list) >= n
+        and np.all(list(map(extractResultKey, data_list[:n])))
+    }
