4647fb9e9f53d90a5fdee51cda8805b94f007fca
CKrawczyk <coleman.krawczyk@gmail.com>
2020-12-04 10:32:06 +0000

    Add ability to track user IDs for question reducer
    This will be used for the "not stargazing live" data reduciton for supernova hunters the weekend of Dec. 12th 2020.

--- panoptes_aggregation/reducers/question_reducer.py
+++ panoptes_aggregation/reducers/question_reducer.py
@@ -1,19 +1,20 @@
 '''
 Question Reducer
 ----------------
-This module porvides functions to reduce the question task extracts from
+This module provides functions to reduce the question task extracts from
 :mod:`panoptes_aggregation.extractors.question_extractor`.
 '''
-from collections import Counter
+from collections import Counter, defaultdict
 from .reducer_wrapper import reducer_wrapper
 
 DEFAULTS = {
-    'pairs': {'default': False, 'type': bool}
+    'pairs': {'default': False, 'type': bool},
+    'track_user_ids': {'default': False, 'type': bool}
 }
 
 
-@reducer_wrapper(defaults_data=DEFAULTS)
-def question_reducer(data_list, pairs=False, **kwargs):
+@reducer_wrapper(defaults_data=DEFAULTS, user_id=True)
+def question_reducer(data_list, pairs=False, track_user_ids=False, **kwargs):
     '''Reduce a list of extracted questions into a "counter" dict
 
     Parameters
@@ -24,18 +25,30 @@ def question_reducer(data_list, pairs=False, **kwargs):
     pairs : bool, optional
         Default `False`. How multiple choice questions are treated.
         When `True` the set of all choices is treated as a single answer
+    track_user_ids : bool, optional
+        Default `False`. Set to `True` to also track the user_ids that gave
+        each answer.
 
     Returns
     -------
     reduction : dict
         A dictionary (formated as a `Counter`) giving the vote count for each
-        `key`
+        `key`. If `user_ids` is `True` it will also contain a list of user_ids
+        for each answer given.
     '''
-    answer_list = []
-    for data in data_list:
+    user_id_list = kwargs.pop('user_id')
+    result = defaultdict(int)
+    for data, user_id in zip(data_list, user_id_list):
         if pairs:
-            answer_list.append('+'.join(sorted(data)))
+            key = '+'.join(sorted(data))
+            result[key] += 1
+            if track_user_ids:
+                key_user_ids = f'user_ids_{key}'
+                result.setdefault(key_user_ids, []).append(user_id)
         else:
-            answer_list += list(data)
-    counter_total = Counter(answer_list)
-    return dict(counter_total)
+            for key in data:
+                result[key] += 1
+                if track_user_ids:
+                    key_user_ids = f'user_ids_{key}'
+                    result.setdefault(key_user_ids, []).append(user_id)
+    return dict(result)
--- panoptes_aggregation/tests/reducer_tests/test_question_reducer.py
+++ panoptes_aggregation/tests/reducer_tests/test_question_reducer.py
@@ -8,6 +8,15 @@ extracted_data = [
     {'b': 1, 'a': 1}
 ]
 
+kwargs_extra_data = {
+    'user_id': [
+        1,
+        2,
+        None,
+        4
+    ]
+}
+
 reduced_data = {
     'a': 3,
     'b': 3,
@@ -19,7 +28,8 @@ TestQuestionReducer = ReducerTestNoProcessing(
     extracted_data,
     reduced_data,
     'Test question reducer',
-    test_name='TestQuestionReducer'
+    test_name='TestQuestionReducer',
+    network_kwargs=kwargs_extra_data
 )
 
 reduced_data_pairs = {
@@ -34,5 +44,47 @@ TestQuestionReducerPairs = ReducerTestNoProcessing(
     reduced_data_pairs,
     'Test question reducer as pairs',
     kwargs={'pairs': True},
-    test_name='TestQuestionReducerPairs'
+    test_name='TestQuestionReducerPairs',
+    network_kwargs=kwargs_extra_data
+)
+
+reduced_data_users = {
+    'a': 3,
+    'b': 3,
+    'c': 1,
+    'user_ids_a': [1, 2, 4],
+    'user_ids_b': [1, None, 4],
+    'user_ids_c': [None]
+}
+
+TestQuestionReducerUsers = ReducerTestNoProcessing(
+    question_reducer,
+    extracted_data,
+    reduced_data_users,
+    'Test question reducer tracking users IDs',
+    kwargs={'track_user_ids': True},
+    test_name='TestQuestionReducerUsers',
+    network_kwargs=kwargs_extra_data
+)
+
+reduced_data_pairs_users = {
+    'a+b': 2,
+    'a': 1,
+    'b+c': 1,
+    'user_ids_a+b': [1, 4],
+    'user_ids_a': [2],
+    'user_ids_b+c': [None]
+}
+
+TestQuestionReducerPairsUsers = ReducerTestNoProcessing(
+    question_reducer,
+    extracted_data,
+    reduced_data_pairs_users,
+    'Test question reducer as pairs and tracking users IDs',
+    kwargs={
+        'pairs': True,
+        'track_user_ids': True
+    },
+    test_name='TestQuestionReducerPairsUsers',
+    network_kwargs=kwargs_extra_data
 )
