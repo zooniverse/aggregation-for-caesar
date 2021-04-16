924d4ea0608d296006be64f859f9da8553d1ffa5
CKrawczyk <coleman.krawczyk@gmail.com>
2021-04-16 14:05:15 +0100

    Ensure null value are handled correctly

--- panoptes_aggregation/tests/extractor_tests/test_dropdown_extractor_v2.py
+++ panoptes_aggregation/tests/extractor_tests/test_dropdown_extractor_v2.py
@@ -26,3 +26,26 @@ TestSimpleDropdown = ExtractorTest(
     'Test simple-dropdown task for classifier v2.0',
     test_name='TestSimpleDropdown'
 )
+
+classification_null = {
+    'metadata': {
+        'classifier_version': '2.0'
+    },
+    'annotations': [
+        {
+            'task': 'T0',
+            'taskType': 'dropdown-simple',
+            'value': None
+        }
+    ]
+}
+
+expected_null = {'value': [{'None': 1}]}
+
+TestSimpleDropdownNull = ExtractorTest(
+    extractors.dropdown_extractor,
+    classification_null,
+    expected_null,
+    'Test simple-dropdown task for classifier v2.0 with no item selected',
+    test_name='TestSimpleDropdownNull'
+)
