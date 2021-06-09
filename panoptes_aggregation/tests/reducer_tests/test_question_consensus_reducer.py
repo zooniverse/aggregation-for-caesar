0e633d7f80ada04800020da78153d53173e2b612
alnah005 <alnah005@umn.edu>
2021-06-09 14:23:58 -0500

    added coverage tests

--- panoptes_aggregation/tests/reducer_tests/test_question_consensus_reducer.py
+++ panoptes_aggregation/tests/reducer_tests/test_question_consensus_reducer.py
@@ -4,14 +4,15 @@ from .base_test_class import ReducerTestNoProcessing
 extracted_data = [
     {'a': 1, 'b': 1},
     {'a': 1},
+    {'d': 1, 'a': 1},
     {'b': 1, 'c': 1},
     {'b': 1, 'a': 1}
 ]
 
 reduced_data = {
     "most_likely": 'a',
-    "num_votes": 3,
-    "agreement": 3 / 7
+    "num_votes": 4,
+    "agreement": 4 / 9
 }
 
 TestQuestionReducer = ReducerTestNoProcessing(
@@ -25,7 +26,7 @@ TestQuestionReducer = ReducerTestNoProcessing(
 reduced_data_pairs = {
     "most_likely": 'a+b',
     "num_votes": 2,
-    "agreement": 2 / 4
+    "agreement": 2 / 5
 }
 
 TestQuestionReducerPairs = ReducerTestNoProcessing(
@@ -35,4 +36,29 @@ TestQuestionReducerPairs = ReducerTestNoProcessing(
     'Test question consensus reducer as pairs',
     kwargs={'pairs': True},
     test_name='TestQuestionConsensusReducerPairs'
+)
+
+reduced_data_no_votes = {
+    "num_votes": 0
+}
+
+TestQuestionReducerPairsNoVotes = ReducerTestNoProcessing(
+    question_consensus_reducer,
+    extracted_data,
+    reduced_data_no_votes,
+    'Test question consensus reducer with no votes',
+    test_name='TestQuestionConsensusReducerNoVotes'
+)
+
+reduced_data_no_votes_pairs = {
+    "num_votes": 0
+}
+
+TestQuestionReducerPairsNoVotesPairs = ReducerTestNoProcessing(
+    question_consensus_reducer,
+    extracted_data,
+    reduced_data_no_votes_pairs,
+    'Test question consensus reducer with no votes as pairs',
+    kwargs={'pairs': True},
+    test_name='TestQuestionConsensusReducerNoVotesPairs'
 )
