4c48fd414fdf556d4445b012c87a8a727ebe5287
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2021-09-14 14:26:17 -0500

    added pluck field extractor

--- panoptes_aggregation/extractors/utilities.py
+++ panoptes_aggregation/extractors/utilities.py
@@ -11,3 +11,30 @@ def annotation_by_task(classification_in):
         ann_by_task[annotation['task']].append(annotation)
     classification['annotations'] = dict(ann_by_task)
     return classification
+
+def pluck_fields(classification, pluck_keys):
+    '''
+        Main function that is called by the pluck_extractor. 
+        Functions similarly to the PluckFieldExtractor
+        on Caesar, where given a mapping between a key and
+        location of the data on the classification JSON,
+        this extractor returns a dictionary with each extracted
+        value given by the corresponding key
+
+    '''
+    answers = defaultdict(list)
+    pluck_key_list = pluck_keys.replace('[','').replace(']','').split(',')
+    for entry in pluck_key_list:
+        key, value = entry.split(':')
+        key_path = value.strip().split('.')
+
+        try:
+            last_value = classification
+            for keyi in key_path:
+                last_value = last_value[keyi.strip()]
+
+            answers[key.strip()] = last_value
+        except KeyError:
+            continue
+
+    return answers
