69b48e431cd2e3e2fc9e292fa8d384eda67c0ea6
Ramanakumar Sankar <rsankar@umn.edu>
2022-04-21 16:59:10 -0500

    created confusion matrix for user skill for k-class tasks. added subject difficulty weighting

--- panoptes_aggregation/reducers/test_user_skill.py
+++ panoptes_aggregation/reducers/test_user_skill.py
@@ -9,27 +9,41 @@ See also: tess_gold_standard_reducer
 '''
 from .reducer_wrapper import reducer_wrapper
 import numpy as np
+from ..feedback_strategies import FEEDBACK_STRATEGIES
 from sklearn.metrics import confusion_matrix
 import sys
+import ast
 
+print2 = lambda x: print(*x, file=sys.stderr, flush=True)
 
 @reducer_wrapper(relevant_reduction=True)
-def test_user_skill_reducer(extracts, relevant_reduction=[], binary=False):
-    confusion_simple, confusion_subject = get_confusion_matrix(extracts, relevant_reduction, binary)
+def test_user_skill_reducer(extracts, relevant_reduction=[], binary=False, null_class='NONE'):
+    if isinstance(binary, str):
+        binary = ast.literal_eval(binary)
+    if binary:
+        classes = ['True', 'False']
+        confusion_simple, confusion_subject = get_confusion_matrix(extracts, relevant_reduction, binary, None)
+    else:
+        confusion_simple, confusion_subject, classes = \
+            get_confusion_matrix(extracts, relevant_reduction, binary, null_class)
 
     #print(confusion_simple, file=sys.stderr)
     #print(confusion_subject, file=sys.stderr)
 
-    per_class_skill = (confusion_subject.diagonal()) / (np.sum(confusion_subject, axis=0) + 1.e-16)
+    # get both the weighted and non-weighted skill
+    weight_per_class_skill = (confusion_subject.diagonal()) / (np.sum(confusion_subject, axis=0) + 1.e-16)
+    per_class_skill = (confusion_simple.diagonal()) / (np.sum(confusion_simple, axis=0) + 1.e-16)
 
     #print(per_class_skill, file=sys.stderr)
 
+    # print2(("class skill: ", per_class_skill))
+
     #sys.stderr.flush()
 
-    return {'skill': per_class_skill[0]}
+    return {'skill': per_class_skill, 'weighted_skill': weight_per_class_skill, 'classes': classes, 'confusion_simple': confusion_simple}
 
 
-def get_confusion_matrix(extracts, relevant_reduction, binary):
+def get_confusion_matrix(extracts, relevant_reduction, binary, null_class):
     if binary:
         # binary always defaults to 2x2 where the second column
         # (gold standard = False) is NaN
@@ -89,39 +103,145 @@ def get_confusion_matrix(extracts, relevant_reduction, binary):
         
         #print(confusion_subject, file=sys.stderr)
         #sys.stderr.flush()
+        return (confusion_simple, confusion_subject)
     else:
         user_classifications = []
+        classes = []
+        strategy = extracts[0]['feedback']['strategy']
+
+        true_key = 'true_'+FEEDBACK_STRATEGIES[strategy][0]
+
+        true_values = []
 
         # first we need a list of labels
         # obtain a list of labels from user classifications
-        for extracti in extracts:
-            user_classifications += [key for key in extracti if isinstance(extracti[key], int)]
+        if strategy in ['singleAnswerQuestion']:
+            for extracti in extracts:
+                user_classifications += [key for key in extracti if isinstance(extracti[key], int)]
+                # get the same from the feedback data
+                true_values.extend(extracti['feedback'][true_key])
 
-        user_classifications = user_classifications
+            classes = user_classifications
+        elif strategy in ['surveySimple']:
+            # for survey simple, the idea is the same
+            # except that the keys have a value of 1 when the 
+            # user selects that class
+            for j, extracti in enumerate(extracts):
+                user_classifications += [key for key in extracti.keys() if isinstance(extracti[key], int)&(extracti[key]==1)]
+                classes              += [key for key in extracti.keys() if isinstance(extracti[key], int)]
+                true_values.extend(extracti['feedback'][true_key])
+                #print2(("classes: ", j, [key for key in extracti.keys() if isinstance(extracti[key], int)&(extracti[key]==1)]))
 
-        # get the same from the feedback data
-        true_values = [extracti['feedback']['true_answer'] for extracti in extracts]
+        #print2(("Extracts: ", extracts))
+
+        #print2(("# extracts: ", len(extracts)))
 
         # get a full list of classes as the union of the two sets of labels
-        classes = np.unique([*np.unique(user_classifications), *np.unique(true_values)])
+        #print2(("Classes: ", np.unique(classes)))
+        #print2(("True_values: ", true_values))
+        classes = np.sort(np.unique([*np.unique(classes), *np.unique(true_values)]))
+        classes = classes.tolist()
+        classes.append(null_class)
 
-        subject_difficulty = 1 - np.asarray([reductioni['data']['difficulty'][0]
-                                             for reductioni in relevant_reduction])
+        #print2(("Merged Classes: ", classes))
+
+
+        #print2(("Classifications: ", user_classifications))
+
+        difficulties = []
+        for reductioni in relevant_reduction:
+            difficulties.append( np.mean(reductioni['data']['difficulty']) )
+
+        subject_difficulty = 1 - np.asarray(difficulties)
 
         # find the easiest subject in the set and set all fully successful
         # subjects to this "easy" score. limit the easy score to 0.05 so that
         # we don't have a runaway growth of easy weights
-        difficulty_min = np.min([np.min(subject_difficulty[subject_difficulty > 0]), 0.05])
+        difficulty_min = np.max([np.min(subject_difficulty[subject_difficulty > 0], initial=0), 0.05])
 
         # limit the difficulty to a mininum of 0.05 so that
         # easy subjects still have some weight
         subject_difficulty[subject_difficulty == 0] = difficulty_min
 
-        mask = (np.asarray(true_values).flatten() == '4') & (np.asarray(user_classifications) == '4')
-        #print(np.asarray(true_values).flatten(), user_classifications, mask, len(mask), len(subject_difficulty), file=sys.stderr)
-        #print(subject_difficulty[mask], np.sum(subject_difficulty[mask]), file=sys.stderr)
+        true_counts = []
+        class_counts = []
+        subject_difficulties = []
 
-        confusion_simple = confusion_matrix(user_classifications, true_values)
-        confusion_subject = confusion_matrix(user_classifications, true_values, sample_weight=subject_difficulty)
+        #print2(("# of difficulties", len(relevant_reduction)))
+        #print2(("# of extracts", len(extracts)))
 
-    return (confusion_simple, confusion_subject)
+        # we will loop through all the extracts and create a list 
+        # of user classified label and a corresponding gold standard
+        # label. then, the confusion matrix is just determined using
+        # the element-wise comparison between the two lists
+        for j, extract in enumerate(extracts):
+
+            # find a list of user classified labels in this extract
+            user_class_i  = [key for key in extract.keys() if isinstance(extract[key], int)&(extract[key]==1)]
+
+            # get a full list of classifications
+            classi = np.sort(np.unique([*np.unique(extract['feedback'][true_key]),
+                                        *np.unique(user_class_i)]))
+            classi = classi.tolist()
+
+            # create a temporary list of classes that will
+            # incorporate both the user selected classes
+            # and the tru classes
+            true_count_i = [null_class]*len(classi)
+            #print2(("Classi: ", extract['feedback'][true_key]))
+
+            # loop through the true classes and populate the corresponding
+            # indices in the list
+            for value in extract['feedback'][true_key]:
+                true_count_i[classi.index(value)] = value
+
+            # do the same for the user classifications
+            class_count_i = [null_class]*len(classi)
+            #print2((j, "user classi: ", user_class_i))
+            for value in user_class_i:
+                class_count_i[classi.index(value)] = value
+            
+            # add both lists to the master list of 
+            # classifications and true classes
+            true_counts.extend(true_count_i)
+            class_counts.extend(class_count_i)
+
+            # also add the subject difficulties for each extract. subject
+            # difficulty per class is not trivial to do, so we will average
+            # the subject difficulty across all the classes in this subject.
+            # easy subjects should get small bump for correct classifications
+            # while difficult subjects will get a huge bump for success
+            # do the opposite for failure scores: easy subject failures should be
+            # penalized more strongly compared to difficulty failures
+            subject_difficulty_i = [subject_difficulty[j]]*len(classi)
+            for class_ind in range(len(classi)):
+                if true_count_i[class_ind] != class_count_i[class_ind]:
+                    subject_difficulty_i[class_ind] = np.max([1. - subject_difficulty_i[class_ind], 0.05])
+
+            subject_difficulties.extend(subject_difficulty_i)
+
+        
+        #mask = (np.array(class_counts)!='NONE')&(np.array(true_counts)!='NONE')
+        #class_counts = np.asarray(class_counts)[mask]
+        #true_counts  = np.asarray(true_counts)[mask]
+        #print("Mask = ", mask, file=sys.stderr)
+        #print2(("Class counts: ", class_counts))
+        #print2(("True counts: ", subject_difficulties))
+
+        # get the simple confusion matrix without the subject difficulty
+        confusion_simple  = confusion_matrix(class_counts, true_counts, labels=classes)
+
+        # get the more complicated confusion matrix accounting for subject difficulty
+        confusion_subject = confusion_matrix(class_counts, true_counts, sample_weight=subject_difficulties,
+                                             labels=classes)
+
+        #print2(("Confusion: ", confusion_simple[classes.index('HIPPOPOTAMUS'),:]))
+        #confusion_subject = confusion_matrix(user_classifications, true_values, sample_weight=subject_difficulty, labels=classes)
+
+        #confusion_simple  = confusion_matrix(user_classifications, true_values, labels=classes)
+        #confusion_subject = confusion_matrix(user_classifications, true_values, sample_weight=subject_difficulty, labels=classes)
+
+        #print2(("Confusion_simple: ", confusion_simple))
+        #print2(("Len conf simple: ", len(confusion_simple)))
+
+        return (confusion_simple, confusion_subject, classes)
