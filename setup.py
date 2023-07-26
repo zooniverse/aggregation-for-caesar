c71483314487087fc8e8154465ab113bec9bc5a7
Ramanakumar Sankar <rsankar@umn.edu>
2023-07-26 10:15:44 -0500

    locking maximum hdbscan version

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
     install_requires=[
         'beautifulsoup4>=4.8.1,<4.13',
         'collatex>=2.2,<2.3',
-        'hdbscan>=0.8.20',
+        'hdbscan>=0.8.20,<=0.8.33',
         'lxml>=4.4,<4.10',
         'numpy>=1.21.5,<1.25.2',
         'packaging>=20.1,<23.1',
