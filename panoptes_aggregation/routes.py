a8875fe607da9bbcb4aa6208efa03f7e660124ed
CKrawczyk <coleman.krawczyk@gmail.com>
2023-09-11 16:08:38 +0100

    fix json loads def

--- panoptes_aggregation/routes.py
+++ panoptes_aggregation/routes.py
@@ -39,7 +39,7 @@ class CustomJSONProvider(JSONProvider):
     def dumps(self, obj, **kwargs):
         return json.dumps(obj, **kwargs, cls=MyEncoder)
 
-    def loads(self, s: str | bytes, **kwargs):
+    def loads(self, s, **kwargs):
         return json.loads(s, **kwargs)
 
 
