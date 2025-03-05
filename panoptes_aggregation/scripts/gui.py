e9c309f8dc7a94e7b594126e50541b32b9bb0b4f
CKrawczyk <coleman.krawczyk@gmail.com>
2025-03-05 15:52:47 +0000

    Change default GUI size
    So it does not go off the bottom of most screens!

--- panoptes_aggregation/scripts/gui.py
+++ panoptes_aggregation/scripts/gui.py
@@ -37,7 +37,7 @@ current_folder = os.path.dirname(os.path.abspath(__file__))
 
 gui = gooey.Gooey(
     program_name="Aggregate",
-    default_size=(1000, 1150),
+    default_size=(1000, 900),
     progress_regex=r"^[a-zA-Z:\s|]+ (\d+)% .+$",
     terminal_font_family="Consolas",
     navigation='TABBED',
