22af09e112a1347eb64e6d77adc72000958061b4
Mark Bouslog <mark@zooniverse.org>
2025-10-03 12:55:05 -0500

    Add NfN extractor conditions for October 2025

--- panoptes_aggregation/extractors/nfn_extractor.py
+++ panoptes_aggregation/extractors/nfn_extractor.py
@@ -109,6 +109,8 @@ def we_dig_bio(parser):
         return 2024
     elif (date.year == 2025) and (date.month == 4) and (10 <= date.day <= 13):
         return 2025
+    elif (date.year == 2025) and (date.month == 10) and (9 <= date.day <= 12):
+        return 2025
     else:
         return None
 
