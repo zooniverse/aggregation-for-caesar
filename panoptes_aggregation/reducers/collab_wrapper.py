20a204bd892bc493e47802665a9642413aac78bc
Molly <molly.burkmar@port.ac.uk>
2026-01-06 19:23:25 +0000

    Add annotations inside clusters_x if block so no clusters are missed

--- panoptes_aggregation/reducers/collab_wrapper.py
+++ panoptes_aggregation/reducers/collab_wrapper.py
@@ -8,8 +8,6 @@ def collab_wrapper(func):
         task_index = kwargs.get('task_index', 0)
         tool_type = kwargs.get('tool_type', 'freehandLine')
 
-        counter = 0
-
         clusters = func(argument, **kwargs)
 
         if not collab:
@@ -21,48 +19,49 @@ def collab_wrapper(func):
                     frame_split = frame_key.split("frame")
                     frame_num = frame_split[1]
 
-                    clusters_x = {}
-                    clusters_y = {}
-
                     for key, value in frame_data.items():
-                        if key.endswith("_cluster_labels"):
+
+                        # shape dependent code goes
+                        # new_dict = shape_dep_fn(...)
+
+                        if key.endswith("_clusters_x"):
                             # classifier v2.0
                             if 'toolIndex' in key:
                                 tool_split = key.split("_toolIndex")
                                 task_key = tool_split[0]
                                 tool_index_split = tool_split[1]
-                                tool_index = tool_index_split.split("_cluster_labels")[0]
+                                tool_index = tool_index_split.split("_clusters_x")[0]
                             # classifier v1.0
                             elif 'tool' in key:
                                 tool_split = key.split("_tool")
                                 task_key = tool_split[0]
                                 tool_index_split = tool_split[1]
-                                tool_index = tool_index_split.split("_cluster_labels")[0]
-                        # shape dependent code goes
-                        # new_dict = shape_dep_fn(...)
-                        if key.endswith("_clusters_x"):
+                                tool_index = tool_index_split.split("_clusters_x")[0]
+
+                            base = key[:-len("_clusters_x")]
                             clusters_x = value
+                            clusters_y = frame_data[f"{base}_clusters_y"]
 
-                        if key.endswith("_clusters_y"):
-                            clusters_y = value
+                            counter = 0
 
-                    for i in range(len(clusters_x)):
-                        annotations = {
-                            'stepKey': step_key,
-                            'taskIndex': task_index,
-                            'taskKey': task_key,
-                            'taskType': 'drawing',
-                            'toolIndex': int(tool_index),
-                            'frame': int(frame_num),
-                            'markID': f'consensus_{counter}',
-                            'toolType': tool_type,
-                            'pathX': clusters_x[i],
-                            'pathY': clusters_y[i]
-                        }
-                        #annotations.update(new_dict)
+                            for i in range(len(clusters_x)):
+                                annotations = {
+                                    'stepKey': step_key,
+                                    'taskIndex': task_index,
+                                    'taskKey': task_key,
+                                    'taskType': 'drawing',
+                                    'toolIndex': int(tool_index),
+                                    'frame': int(frame_num),
+                                    'markID': f'consensus_{counter}',
+                                    'toolType': tool_type,
+                                    'pathX': clusters_x[i],
+                                    'pathY': clusters_y[i]
+                                }
 
-                        clusters.setdefault('data', []).append(annotations)
-                        counter += 1
+                                # annotations.update(new_dict)
+
+                                clusters.setdefault('data', []).append(annotations)
+                                counter += 1
 
         return clusters
 
