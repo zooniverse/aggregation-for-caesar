436a80a3eca4e0412e96be31332bf495f38a892f
Molly <molly.burkmar@port.ac.uk>
2026-03-24 18:58:01 +0000

    Add reducer test with OPTICS when collab: True

--- panoptes_aggregation/tests/reducer_tests/test_shape_reducer_rectangle.py
+++ panoptes_aggregation/tests/reducer_tests/test_shape_reducer_rectangle.py
@@ -167,6 +167,138 @@ TestShapeReducerRectangleOptics = ReducerTest(
     test_name='TestShapeReducerRectangleOptics'
 )
 
+reduced_data_collab_true = {
+'data': [
+        {
+            "frame": 0,
+            "markID": "consensus_0",
+            "min_threshold": 0,
+            "stepKey": "S0",
+            "taskIndex": 0,
+            "taskKey": "T0",
+            "taskType": "rectangle",
+            "threshold": "0.75",
+            "toolIndex": 0,
+            "toolType": "freehandLine",
+            "rec_x": [
+                0.0, 100.0
+            ],
+            "rec_y": [
+                0.0, 100.0
+            ],
+            "rec_width": [
+                50.0, 10.0
+            ],
+            "rec_height": [
+                60.0, 20.0
+            ]
+        },
+        {
+            "frame": 0,
+            "markID": "consensus_0",
+            "min_threshold": 0,
+            "stepKey": "S0",
+            "taskIndex": 0,
+            "taskKey": "T0",
+            "taskType": "rectangle",
+            "threshold": "0.75",
+            "toolIndex": 1,
+            "toolType": "freehandLine",
+            "rec_x": [
+                0.0, 100.0
+            ],
+            "rec_y": [
+                100.0, 0.0
+            ],
+            "rec_width": [
+                10.0, 50.0
+            ],
+            "rec_height": [
+                50.0, 10.0
+            ]
+        },
+{
+            "frame": 1,
+            "markID": "consensus_0",
+            "min_threshold": 0,
+            "stepKey": "S0",
+            "taskIndex": 0,
+            "taskKey": "T0",
+            "taskType": "rectangle",
+            "threshold": "0.75",
+            "toolIndex": 1,
+            "toolType": "freehandLine",
+            "rec_x": [
+                50.0
+            ],
+            "rec_y": [
+                50.0
+            ],
+            "rec_width": [
+                50.0
+            ],
+            "rec_height": [
+                50.0
+            ]
+        }
+        ],
+    'frame0': {
+        'T0_tool0_rectangle_x': [0.0, 100.0, 0.0, 100.0],
+        'T0_tool0_rectangle_y': [0.0, 100.0, 0.0, 100.0],
+        'T0_tool0_rectangle_width': [50.0, 10.0, 50.0, 10.0],
+        'T0_tool0_rectangle_height': [60.0, 20.0, 60.0, 20.0],
+        'T0_tool0_cluster_labels': [0, 1, 0, 1],
+        'T0_tool0_clusters_count': [2, 2],
+        'T0_tool0_clusters_x': [0.0, 100.0],
+        'T0_tool0_clusters_y': [0.0, 100.0],
+        'T0_tool0_clusters_width': [50.0, 10.0],
+        'T0_tool0_clusters_height': [60.0, 20.0],
+        'T0_tool1_rectangle_x': [0.0, 100.0, 0.0, 100.0],
+        'T0_tool1_rectangle_y': [100.0, 0.0, 100.0, 0.0],
+        'T0_tool1_rectangle_width': [10.0, 50.0, 10.0, 50.0],
+        'T0_tool1_rectangle_height': [50.0, 10.0, 50.0, 10.0],
+        'T0_tool1_cluster_labels': [0, 1, 0, 1],
+        'T0_tool1_clusters_count': [2, 2],
+        'T0_tool1_clusters_x': [0.0, 100.0],
+        'T0_tool1_clusters_y': [100.0, 0.0],
+        'T0_tool1_clusters_width': [10.0, 50.0],
+        'T0_tool1_clusters_height': [50.0, 10.0]
+    },
+    'frame1': {
+        'T0_tool0_rectangle_x': [20.0],
+        'T0_tool0_rectangle_y': [20.0],
+        'T0_tool0_rectangle_width': [20.0],
+        'T0_tool0_rectangle_height': [20.0],
+        'T0_tool0_cluster_labels': [-1],
+        'T0_tool1_rectangle_x': [50.0, 50.0],
+        'T0_tool1_rectangle_y': [50.0, 50.0],
+        'T0_tool1_rectangle_width': [50.0, 50.0],
+        'T0_tool1_rectangle_height': [50.0, 50.0],
+        'T0_tool1_cluster_labels': [0, 0],
+        'T0_tool1_clusters_count': [2],
+        'T0_tool1_clusters_x': [50.0],
+        'T0_tool1_clusters_y': [50.0],
+        'T0_tool1_clusters_width': [50.0],
+        'T0_tool1_clusters_height': [50.0]
+    }
+}
+
+TestShapeReducerRectangleOpticsCollabTrue = ReducerTest(
+    shape_reducer_optics,
+    process_data_optics,
+    extracted_data,
+    processed_data,
+    reduced_data_collab_true,
+    'Test shape rectangle reducer with OPTICS when collab: True',
+    network_kwargs=kwargs_extra_data,
+    pkwargs={'shape': 'rectangle'},
+    kwargs={
+        'min_samples': 2,
+        'collab': True
+    },
+    test_name='TestShapeReducerRectangleOpticsCollabTrue'
+)
+
 reduced_data_hdbscan = copy.deepcopy(reduced_data)
 reduced_data_hdbscan['frame0']['T0_tool0_cluster_probabilities'] = [1.0, 1.0, 1.0, 1.0]
 reduced_data_hdbscan['frame0']['T0_tool1_cluster_probabilities'] = [1.0, 1.0, 1.0, 1.0]
