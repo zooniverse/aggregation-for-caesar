8ae1bd66785b9839dbf99a09b13347b3f85cefb3
Mark Bouslog <mcbouslog@gmail.com>
2026-04-02 14:00:48 -0500

    Add NfN extractor conditions for April 2026

--- panoptes_aggregation/extractors/nfn_extractor.py
+++ panoptes_aggregation/extractors/nfn_extractor.py
@@ -111,6 +111,8 @@ def we_dig_bio(parser):
         return 2025
     elif (date.year == 2025) and (date.month == 10) and (9 <= date.day <= 12):
         return 2025
+    elif (date.year == 2026) and (date.month == 4) and (9 <= date.day <= 12):
+        return 2026
     else:
         return None
 
