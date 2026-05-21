4d7d5d740513f212d70c5d4c6d4b49e2bc1bb6cf
CKrawczyk <coleman.krawczyk@gmail.com>
2026-05-21 13:49:06 +0100

    Update test for shape extractor point

--- panoptes_aggregation/tests/extractor_tests/test_shape_extractor_point.py
+++ panoptes_aggregation/tests/extractor_tests/test_shape_extractor_point.py
@@ -102,3 +102,115 @@ TestShapePointOneTool = ExtractorTest(
     },
     test_name='TestShapePointOneTool'
 )
+
+classification_v2 = {
+    'annotations': [
+        {
+            'task': 'T0',
+            'taskType': 'drawing',
+            'value': [
+                {
+                    'toolIndex': 0,
+                    'toolType': 'point',
+                    'frame': 0,
+                    'x': 0,
+                    'y': 5
+                },
+                {
+                    'toolIndex': 0,
+                    'toolType': 'point',
+                    'frame': 0,
+                    'x': 10,
+                    'y': 15
+                },
+                {
+                    'toolIndex': 1,
+                    'toolType': 'point',
+                    'frame': 0,
+                    'x': 20,
+                    'y': 25
+                },
+                {
+                    'toolIndex': 0,
+                    'toolType': 'point',
+                    'frame': 1,
+                    'x': 30,
+                    'y': 35
+                }
+            ]
+        }
+    ],
+    'metadata': {
+        'classifier_version': '2.0'
+    }
+}
+
+expected_v2 = {
+    'classifier_version': '2.0',
+    'frame0': {
+        'T0_toolIndex0_x': [0, 10],
+        'T0_toolIndex0_y': [5, 15],
+        'T0_toolIndex1_x': [20],
+        'T0_toolIndex1_y': [25]
+    },
+    'frame1': {
+        'T0_toolIndex0_x': [30],
+        'T0_toolIndex0_y': [35]
+    }
+}
+
+TestShapePoint_v2 = ExtractorTest(
+    extractors.shape_extractor,
+    classification_v2,
+    expected_v2,
+    'Test shape point V2',
+    kwargs={'shape': 'point'},
+    test_name='TestShapePoint_v2'
+)
+
+TestShapePointTask_v2 = ExtractorTest(
+    extractors.shape_extractor,
+    classification_v2,
+    expected_v2,
+    'Test shape point V2 with task specified',
+    kwargs={
+        'shape': 'point',
+        'task': 'T0'
+    },
+    test_name='TestShapePointTask_v2'
+)
+
+TestShapePointAllTools_v2 = ExtractorTest(
+    extractors.shape_extractor,
+    classification_v2,
+    expected_v2,
+    'Test shape point V2 with all tools specified',
+    kwargs={
+        'shape': 'point',
+        'task': 'T0',
+        'tools': [0, 1]
+    },
+    test_name='TestShapePointAllTools_v2'
+)
+
+expected_v2_0 = {
+    'classifier_version': '2.0',
+    'frame0': {
+        'T0_toolIndex0_x': expected_v2['frame0']['T0_toolIndex0_x'],
+        'T0_toolIndex0_y': expected_v2['frame0']['T0_toolIndex0_y']
+    },
+    'frame1': expected_v2['frame1']
+}
+
+TestShapePointOneTool_v2 = ExtractorTest(
+    extractors.shape_extractor,
+    classification_v2,
+    expected_v2_0,
+    'Test shape point V2 with one tool specified',
+    kwargs={
+        'shape': 'point',
+        'task': 'T0',
+        'tools': [0]
+    },
+    test_name='TestShapePointOneTool_v2'
+)
