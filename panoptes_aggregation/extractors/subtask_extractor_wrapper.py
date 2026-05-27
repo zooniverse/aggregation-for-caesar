5f485626e713448e93c6c065b83916df9a2515f6
CKrawczyk <coleman.krawczyk@gmail.com>
2026-05-27 13:48:43 +0100

    Initial pass at a unified v1.0 and v2.0 subtask extractor
    The classifier version is not stored at the workflow level, this means that the config script can't just swap out the "details config" between the old style and the new one and expect everything to work. (I.e. one workflow can have both v1.0 and v2.0 classifications in it).
    
    Instead these changes make two new functions to convert the detail config between the two styles as needed.

--- panoptes_aggregation/extractors/subtask_extractor_wrapper.py
+++ panoptes_aggregation/extractors/subtask_extractor_wrapper.py
@@ -1,6 +1,7 @@
 from functools import wraps
 import copy
 from panoptes_aggregation import extractors
+from panoptes_aggregation.details_convert import details_flatten, details_unflatten
 from packaging import version
 
 
@@ -19,9 +20,12 @@ def subtask_wrapper(func):
                 if annotation.get('taskType', 'drawing') in ['drawing', 'dataVisAnnotation']:
                     data_drawing['annotations'].append(annotation)
                 else:
-                    subtask_key = annotation['task']
-                    subtask_mark = annotation['markIndex']
-                    data_subtask[(subtask_key, subtask_mark)] = annotation
+                    # if the task is specified this should not be needed
+                    # but it is good to be safe
+                    if 'markIndex' in annotation:
+                        subtask_key = annotation['task']
+                        subtask_mark = annotation['markIndex']
+                        data_subtask[(subtask_key, subtask_mark)] = annotation
             output = func(data_drawing, **kwargs)
             output['classifier_version'] = str(classifier_version)
         else:
@@ -29,6 +33,8 @@ def subtask_wrapper(func):
         if details_functions is not None:
             if classifier_version < version.parse('2.0'):
                 # old classifier version
+                # ensure details config matches v1.0 style
+                details_functions_v1 = details_unflatten(details_functions)
                 blank_annotation = {'annotations': {'ST': []}}
                 for annotation in data['annotations']:
                     task_key = annotation['task']
@@ -36,19 +42,21 @@ def subtask_wrapper(func):
                         key_prefix = '{0}_tool{1}'.format(task_key, value['tool'])
                         key = '{0}_details'.format(key_prefix)
                         frame = 'frame{0}'.format(value['frame'])
-                        if key_prefix in details_functions:
+                        if key_prefix in details_functions_v1:
                             output[frame].setdefault(key, []).append([])
                             for ddx, detail in enumerate(value['details']):
                                 mock_annotation = copy.deepcopy(blank_annotation)
                                 mock_annotation['annotations']['ST'].append(detail)
-                                if details_functions[key_prefix][ddx] in extractors.extractors:
-                                    extractor = extractors.extractors[details_functions[key_prefix][ddx]]
+                                if details_functions_v1[key_prefix][ddx] in extractors.extractors:
+                                    extractor = extractors.extractors[details_functions_v1[key_prefix][ddx]]
                                     detail_extract = extractor(mock_annotation, no_version=True)
                                     output[frame][key][-1].append(detail_extract)
                                 else:
                                     output[frame][key][-1].append('No extractor for this subtask type')
             else:
                 # new classifier version
+                # ensure details config matches v2.0 style
+                details_functions_v2 = details_flatten(details_functions)
                 for annotation in data_drawing['annotations']:
                     task_key = annotation['task']
                     for vdx, value in enumerate(annotation['value']):
@@ -56,13 +64,16 @@ def subtask_wrapper(func):
                         for detail in value['details']:
                             subtask = detail['task']
                             subtask_key = '{0}_toolIndex{1}_subtask{2}'.format(*subtask.split('.'))
-                            if subtask_key in details_functions:
+                            if subtask_key in details_functions_v2:
                                 output[frame].setdefault(subtask_key, [])
-                                extractor = extractors.extractors[details_functions[subtask_key]]
-                                subtask_annotation = {'annotations': {
-                                    subtask_key: [data_subtask[(subtask, vdx)]]
-                                }}
-                                detail_extract = extractor(subtask_annotation, no_version=True)
-                                output[frame][subtask_key].append(detail_extract)
+                                if details_functions_v2[subtask_key] in extractors.extractors:
+                                    extractor = extractors.extractors[details_functions_v2[subtask_key]]
+                                    subtask_annotation = {'annotations': {
+                                        subtask_key: [data_subtask[(subtask, vdx)]]
+                                    }}
+                                    detail_extract = extractor(subtask_annotation, no_version=True)
+                                    output[frame][subtask_key].append(detail_extract)
+                                else:
+                                    output[frame][subtask_key].append('No extractor for this subtask type')
         return output
     return wrapper
---
+++ panoptes_aggregation/tests/utility_tests/test_details_convert.py
@@ -0,0 +1,44 @@
+import unittest
+from panoptes_aggregation.details_convert import details_flatten, details_unflatten
+
+flat_example = {
+    'T0_toolIndex0_subtask1': 'dropdown_extractor',
+    'T0_toolIndex0_subtask0': 'question_extractor',
+    'T0_toolIndex0_subtask2': None,
+    'T0_toolIndex1_subtask0': 'question_extractor',
+    'T0_toolIndex1_subtask1': 'dropdown_extractor'
+}
+
+unflat_example = {
+    'T0_tool0': [
+        'question_extractor',
+        'dropdown_extractor',
+        None
+    ],
+    'T0_tool1': [
+        'question_extractor',
+        'dropdown_extractor'
+    ]
+}
+
+
+class TestDetailsConvert(unittest.TestCase):
+    def test_flatten(self):
+        '''Test v1.0 to v2.0 details config conversion'''
+        result = details_flatten(unflat_example)
+        self.assertDictEqual(result, flat_example)
+
+    def test_flatten_no_op(self):
+        '''Test v2.0 to v2.0 details config conversion'''
+        result = details_flatten(flat_example)
+        self.assertDictEqual(result, flat_example)   
+
+    def test_unflatten(self):
+        '''Test v2.0 to v1.0 details config conversion'''
+        result = details_unflatten(flat_example)
+        self.assertDictEqual(result, unflat_example)
+
+    def test_unflatten_no_op(self):
+        '''Test v1.0 to v1.0 details config conversion'''
+        result = details_unflatten(unflat_example)
+        self.assertDictEqual(result, unflat_example)
