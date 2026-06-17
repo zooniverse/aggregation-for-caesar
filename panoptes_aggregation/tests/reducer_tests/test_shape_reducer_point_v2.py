069e09ce00de6b6370fecc46aff1cbbd1d3bcf15
CKrawczyk <coleman.krawczyk@gmail.com>
2026-06-17 14:32:06 +0100

    Update point test with collab

--- panoptes_aggregation/tests/reducer_tests/test_shape_reducer_point_v2.py
+++ panoptes_aggregation/tests/reducer_tests/test_shape_reducer_point_v2.py
@@ -60,6 +60,7 @@ processed_data = {
     'shape': 'point',
     'symmetric': False,
     'classifier_version': '2.0',
+    'n_classifications': 5,
     'frame0': {
         'T0_toolIndex0': [
             (0.0, 0.0),
@@ -165,4 +166,119 @@ TestShapeReducerPointHdbscan_v2 = ReducerTest(
         'allow_single_cluster': True
     },
     test_name='TestShapeReducerPointHdbscan_v2'
+)
+
+data_collab = [
+    {
+        'stepKey': 'S0',
+        'taskIndex': 0,
+        'taskKey': 'T0',
+        'taskType': 'drawing',
+        'toolIndex': 0,
+        'frame': 0,
+        'markId': 'collab_frame0_T0_toolIndex0_0',
+        'toolType': 'point',
+        'x': 0.0,
+        'y': 0.0
+    }, {
+        'stepKey': 'S0',
+        'taskIndex': 0,
+        'taskKey': 'T0',
+        'taskType': 'drawing',
+        'toolIndex': 0,
+        'frame': 0,
+        'markId': 'collab_frame0_T0_toolIndex0_1',
+        'toolType': 'point',
+        'x': 100.0,
+        'y': 100.0
+    }, {
+        'stepKey': 'S0',
+        'taskIndex': 0,
+        'taskKey': 'T0',
+        'taskType': 'drawing',
+        'toolIndex': 1,
+        'frame': 0,
+        'markId': 'collab_frame0_T0_toolIndex1_0',
+        'toolType': 'point',
+        'x': 0.0,
+        'y': 100.0
+    }, {
+        'stepKey': 'S0',
+        'taskIndex': 0,
+        'taskKey': 'T0',
+        'taskType': 'drawing',
+        'toolIndex': 1,
+        'frame': 0,
+        'markId': 'collab_frame0_T0_toolIndex1_1',
+        'toolType': 'point',
+        'x': 100.0,
+        'y': 0.0
+    }, {
+        'stepKey': 'S0',
+        'taskIndex': 0,
+        'taskKey': 'T0',
+        'taskType': 'drawing',
+        'toolIndex': 1,
+        'frame': 1,
+        'markId': 'collab_frame1_T0_toolIndex1_0',
+        'toolType': 'point',
+        'x': 50.0,
+        'y': 50.0
+    }
+]
+
+reduced_data_collab = copy.deepcopy(reduced_data)
+reduced_data_collab['data'] = data_collab
+reduced_data_hdbscan_collab = copy.deepcopy(reduced_data_hdbscan)
+reduced_data_hdbscan_collab['data'] = data_collab
+
+TestShapeReducerPoint_v2_collab = ReducerTest(
+    shape_reducer_dbscan,
+    process_data_dbscan,
+    extracted_data,
+    processed_data,
+    reduced_data_collab,
+    'Test shape point V2.0 reducer with DBSCAN with collab',
+    network_kwargs=kwargs_extra_data,
+    pkwargs={'shape': 'point'},
+    kwargs={
+        'eps': 5,
+        'min_samples': 2,
+        'collab': True
+    },
+    test_name='TestShapeReducerPoint_v2_collab'
+)
+
+TestShapeReducerPointOptics_v2_collab = ReducerTest(
+    shape_reducer_optics,
+    process_data_optics,
+    extracted_data,
+    processed_data,
+    reduced_data_collab,
+    'Test shape point V2.0 reducer with OPTICS with collab',
+    network_kwargs=kwargs_extra_data,
+    pkwargs={'shape': 'point'},
+    kwargs={
+        'min_samples': 2,
+        'collab': True
+    },
+    test_name='TestShapeReducerPointOptics_v2_collab'
+)
+
+TestShapeReducerPointHdbscan_v2_collab = ReducerTest(
+    shape_reducer_hdbscan,
+    process_data_hdbscan,
+    extracted_data,
+    processed_data,
+    reduced_data_hdbscan_collab,
+    'Test shape point V2.0 reducer with HDBSCAN with collab',
+    network_kwargs=kwargs_extra_data,
+    pkwargs={'shape': 'point'},
+    kwargs={
+        'min_cluster_size': 2,
+        'min_samples': 1,
+        'allow_single_cluster': True,
+        'collab': True
+    },
+    test_name='TestShapeReducerPointHdbscan_v2_collab'
 )
