b225c8473e0f315caca33f8884ca727afe6f2a7e
CKrawczyk <coleman.krawczyk@gmail.com>
2026-06-17 15:00:12 +0100

    Update line test with collab
    Again only one test added as all the various reducers use the same wrapper under the hood.

--- panoptes_aggregation/tests/reducer_tests/test_shape_reducer_line_v2.py
+++ panoptes_aggregation/tests/reducer_tests/test_shape_reducer_line_v2.py
@@ -74,6 +74,7 @@ processed_data = {
     'shape': 'line',
     'symmetric': False,
     'classifier_version': '2.0',
+    'n_classifications': 5,
     'frame0': {
         'T0_toolIndex0': [
             (0.0, 0.0, 50.0, 60.0),
@@ -153,6 +154,82 @@ TestShapeReducerLine_v2 = ReducerTest(
     test_name='TestShapeReducerLine_v2'
 )
 
+data_collab = [
+    {
+        'stepKey': 'S0',
+        'taskIndex': 0,
+        'taskKey': 'T0',
+        'taskType': 'drawing',
+        'toolIndex': 0,
+        'frame': 0,
+        'markId': 'collab_frame0_T0_toolIndex0_0',
+        'toolType': 'line',
+        'x1': 0.0,
+        'y1': 0.0,
+        'x2': 50.0,
+        'y2': 60.0
+    }, {
+        'stepKey': 'S0',
+        'taskIndex': 0,
+        'taskKey': 'T0',
+        'taskType': 'drawing',
+        'toolIndex': 0,
+        'frame': 0,
+        'markId': 'collab_frame0_T0_toolIndex0_1',
+        'toolType': 'line',
+        'x1': 100.0,
+        'y1': 100.0,
+        'x2': 10.0,
+        'y2': 20.0
+    }, {
+        'stepKey': 'S0',
+        'taskIndex': 0,
+        'taskKey': 'T0',
+        'taskType': 'drawing',
+        'toolIndex': 1,
+        'frame': 0,
+        'markId': 'collab_frame0_T0_toolIndex1_0',
+        'toolType': 'line',
+        'x1': 0.0,
+        'y1': 100.0,
+        'x2': 10.0,
+        'y2': 50.0
+    }, {
+        'stepKey': 'S0',
+        'taskIndex': 0,
+        'taskKey': 'T0',
+        'taskType': 'drawing',
+        'toolIndex': 1,
+        'frame': 0,
+        'markId': 'collab_frame0_T0_toolIndex1_1',
+        'toolType': 'line',
+        'x1': 100.0,
+        'y1': 0.0,
+        'x2': 50.0,
+        'y2': 10.0
+    }
+]
+
+reduced_data_collab = copy.deepcopy(reduced_data)
+reduced_data_collab['data'] = data_collab
+
+TestShapeReducerLine_v2_collab = ReducerTest(
+    shape_reducer_dbscan,
+    process_data_dbscan,
+    extracted_data,
+    processed_data,
+    reduced_data_collab,
+    'Test shape line V2.0 reducer with DBSCAN with collab',
+    network_kwargs=kwargs_extra_data,
+    pkwargs={'shape': 'line'},
+    kwargs={
+        'eps': 5,
+        'min_samples': 2,
+        'collab': True
+    },
+    test_name='TestShapeReducerLine_v2_collab'
+)
+
 reduced_data_optics = copy.deepcopy(reduced_data)
 reduced_data_optics['frame1']['T0_toolIndex1_cluster_labels'] = [0, 0]
 reduced_data_optics['frame1']['T0_toolIndex1_clusters_count'] = [2]
@@ -180,6 +257,7 @@ processed_data_symmetric = {
     'shape': 'line',
     'symmetric': True,
     'classifier_version': '2.0',
+    'n_classifications': 5,
     'frame0': {
         'T0_toolIndex0': [
             (0.0, 0.0, 50.0, 60.0),
